bcba866f6442eba20ee92fda2955d8a44a47870b
adaliz1 <ada.melgarejo@fiuni.edu.py>
2023-03-07 09:56:39 -0300

    pantalla listar colegios con datos estatitos

--- profefolio-frontend/src/pages/administrador/colegios/ListarColegios.jsx
+++ profefolio-frontend/src/pages/administrador/colegios/ListarColegios.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import Table from 'react-bootstrap/Table';
 import './ListarColegios.css'
+import { useNavigate } from "react-router-dom";
+import {BiArrowBack} from "react-icons/bi"
+import {BiPencil} from "react-icons/bi"
+import {BiTrash} from "react-icons/bi"
+import {BiInfoCircle} from "react-icons/bi"
+
+
 const ListarColegios = () => {
+  const navigate = useNavigate()
   const colegios = [
     { id: 0, nombre: "Marcelina Bogado", direccion: "Coronel Bogado" },
     { id: 1, nombre: "San Jose", direccion: "Coronel Bogado" },
@@ -12,7 +20,7 @@ const ListarColegios = () => {
     <>
     <div className="container-principal">
       <div className="nombre-pagina">
-        <button>volver</button>
+        <button className="button-back" onClick={()=>{navigate('/administrador')}}><BiArrowBack/></button>
         <span>Colegios</span>
       </div>
       <div className="table-principal" >
@@ -20,17 +28,17 @@ const ListarColegios = () => {
         <thead>
           <tr>
 
-            <th>Nombre</th>
-            <th>Direccion</th>
-            <th>Acciones</th>
+            <th id="table-border">Nombre</th>
+            <th id="table-border">Direccion</th>
+            <th className="actions-th" id="table-border">Acciones</th>
           </tr>
         </thead>
         <tbody >
         {colegios.map((colegio, index) => (  
-              <tr data-index={index}>  
-                <td>{colegio.nombre}</td>  
-                <td>{colegio.direccion}</td>
-                <td><button>hola</button></td>  
+              <tr key={colegio.id}>  
+                <td id="table-border" >{colegio.nombre}</td>  
+                <td id="table-border">{colegio.direccion}</td>
+                <td className="actions-td" id="table-border"><button className="information-buttons"><BiTrash /></button> <button className="information-buttons"><BiPencil /> </button> <button className="information-buttons"><BiInfoCircle/></button></td>  
               </tr>  
             ))} 
         </tbody>
