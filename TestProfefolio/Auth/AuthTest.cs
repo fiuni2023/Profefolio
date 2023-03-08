876e5236c7988f7a81141f7936133864cc45476c
Carlos Torres <cctc2013@gmail.com>
2023-03-07 21:30:27 -0300

    Creado y agregado Test para Login

---
+++ TestProfefolio/Auth/AuthTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using profefolio.Controllers;
+using profefolio.Models.DTOs.Auth;
+using profefolio.Repository;
+
+namespace TestProfefolio.Auth;
+
+public class AuthTest
+{
+    private readonly Mock<IAuth> _authService = new Mock<IAuth>();
+    private readonly AuthController _authController;
+
+
+    public AuthTest()
+    {
+        _authController = new AuthController(_authService.Object);
+    }
+    [Fact]
+    public async Task LoginResponseOkTest()
+    {
+        var login = new Login()
+        {
+            Email = "Carlos.Torres123@mail.com",
+            Password = "Carlos.Torres123"
+        };
+
+        var persona = new AuthPersonaDTO()
+        {
+            Email = "Carlos.Torres123@mail.com",
+            Roles = new List<string>()
+            {
+                "Master"
+            }
+        };
+
+        _authService.Setup(s => s.Login(login))
+            .ReturnsAsync(persona);
+
+        var result = await _authController.Login(login);
+
+        Assert.IsType<OkObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task LoginResponseValueTest()
+    {
+        var login = new Login()
+        {
+            Email = "Carlos.Torres123@mail.com",
+            Password = "Carlos.Torres123"
+        };
+
+        var persona = new AuthPersonaDTO()
+        {
+            Email = "Carlos.Torres123@mail.com",
+            Roles = new List<string>()
+            {
+                "Master"
+            }
+        };
+
+        _authService.Setup(s => s.Login(login))
+            .ReturnsAsync(persona);
+
+        var result = await _authController.Login(login);
+
+        Assert.IsType<AuthPersonaDTO>(result.Result);
+    }
+    
+}
