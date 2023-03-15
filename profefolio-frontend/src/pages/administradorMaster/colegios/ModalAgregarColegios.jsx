d6d3a2be54fbf80e86db8daf4a5b2ac562d204e1
adaliz1 <ada.melgarejo@fiuni.edu.py>
2023-03-14 22:22:31 -0300

    cambios en modal

--- profefolio-frontend/src/pages/administradorMaster/colegios/ModalAgregarColegios.jsx
+++ profefolio-frontend/src/pages/administradorMaster/colegios/ModalAgregarColegios.jsx
@@ -1,12 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 import "./ModalAgregarColegios.css"
 import { BsFillPlusCircleFill } from "react-icons/bs"
 import axios from "axios";
 function ModalDialog() {
     const [nombreColegio, setNombreColegio] = useState(null);
+    const [idAdmin, setIdAdmin] = useState(null);
+    const [administradores, setAdministradores] = useState([]);
+    //Get administadores
+    var config = {
+        method: 'get',
+        url: 'https://localhost:7063/api/administrador/page/0',
+        headers: {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQ2FybG9zLlRvcnJlczEyM0BtYWlsLmNvbSIsImp0aSI6IjY3NjdkMTZhLTQxNWItNGRhZC1iMTRjLTBjZjZkNDI2ZDRmYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hc3RlciIsImV4cCI6MTY3ODg1MDUwOSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.R_dHv3w5gutvZzBcO2x2px8RyqIdVbgrlL2eck-fXAM',
+            'Content-Type': 'application/json'
+        },
+       
 
-    /*var config = {
+    };
+    useEffect(() => {
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                setAdministradores(response.data.dataList);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },[])
+   /* var config = {
         method: 'post',
         url: 'https://localhost:7063/api/Colegios',
         headers: {
@@ -14,11 +36,12 @@ function ModalDialog() {
             'Content-Type': 'application/json'
         },
         //data: data
-    };
+    };*/
     function guardarDatos(){
-        setNombreColegio()
+        console.log(nombreColegio);
+        console.log(idAdmin);
 
-    }*/
+    }
     /*
     var axios = require('axios');
     var data = JSON.stringify({
@@ -56,10 +79,14 @@ function ModalDialog() {
                     <div>
                         <form>
                             <label for="colegio-nombre"><strong> Colegio</strong></label><br />
-                            <input required type="text" id="input-colegio" name="colegio-nombre"></input><br />
+                            <input required type="text" id="input-colegio" name="colegio-nombre" onChange={event => setNombreColegio(event.target.value)}></input><br />
                             <label for="administrador"><strong> Administrador</strong></label><br />
-                            <input required placeholder='Nombre' type="text" id="input-admin" name="nombre-administrador" ></input>
-                            <input required type="text" id="input-apellido" placeholder='Apellido' name="apellido-administrador"></input>
+
+                            <select name="admin" value={idAdmin} onChange={e => setIdAdmin(e.target.value)}>
+                                {administradores.map((administrador) =>
+                                    <option key={administrador.id} value={administrador.id}>{administrador.nombre} {administrador.apellido}</option>
+                                )}
+                            </select>
                         </form>
                     </div>
 
@@ -68,7 +95,7 @@ function ModalDialog() {
                     <Button className="btn btn-light btn-sm" onClick={closeModal}>
                         Cancelar
                     </Button>
-                    <Button className='btn-guardar btn-sm' onClick={closeModal}>
+                    <Button className='btn-guardar btn-sm' onClick={guardarDatos()}>
                         Guardar
                     </Button>
                 </Modal.Footer>
