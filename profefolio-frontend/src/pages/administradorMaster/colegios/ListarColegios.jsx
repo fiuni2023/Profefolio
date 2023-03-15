91b6e6bec6e2b9494f1b1e8180d369a8b570eb0b
adaliz1 <ada.melgarejo@fiuni.edu.py>
2023-03-15 14:46:51 -0300

    paginacion en mostrar colegios y carga de colegios lista

--- profefolio-frontend/src/pages/administradorMaster/colegios/ListarColegios.jsx
+++ profefolio-frontend/src/pages/administradorMaster/colegios/ListarColegios.jsx
@@ -17,7 +17,7 @@ const ListarColegios = () => {
   const [colegios, setColegios] = useState([]);
   var config = {
     method: 'get',
-    url: `https://localhost:7063/api/Colegios/page/${currentPage}`,
+    url: `https://localhost:7063/api/ColegiosFull/page/0`,
     headers: {
       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoicHJ1ZWJhQGdtYWlsLmNvbSIsImp0aSI6IjE5OGRjNGRhLTgxMzQtNDkwMC04NTNjLTNlZjY5MDE0ZGVhZCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluaXN0cmFkb3IgZGUgQ29sZWdpbyIsImV4cCI6MTY3ODc3Mzk2NywiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.NyBXTa2FMx9JinQe9GQdOL2LXAJ90JxS4DiKQaR5OQ8'
     }
@@ -25,7 +25,6 @@ const ListarColegios = () => {
   useEffect(() => {
     axios(config)
       .then(function (response) {
-        console.log(JSON.stringify(response.data));
         setColegios(response.data.dataList); //Guarda los datos
         setTotalPage(response.data.totalPage);//Total de Paginas
         setCurrentPage(response.data.currentPage)//Actualiza la pagina en donde estan los datos
@@ -36,9 +35,31 @@ const ListarColegios = () => {
       });
 
   }, [])
+
+const recargaDatos=(id)=>{
+ var config = {
+    method: 'get',
+    url: `https://localhost:7063/api/ColegiosFull/page/${id}`,
+    headers: {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoicHJ1ZWJhQGdtYWlsLmNvbSIsImp0aSI6IjE5OGRjNGRhLTgxMzQtNDkwMC04NTNjLTNlZjY5MDE0ZGVhZCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFkbWluaXN0cmFkb3IgZGUgQ29sZWdpbyIsImV4cCI6MTY3ODc3Mzk2NywiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.NyBXTa2FMx9JinQe9GQdOL2LXAJ90JxS4DiKQaR5OQ8'
+    }
+  };
+    axios(config)
+      .then(function (response) {
+       
+        setColegios(response.data.dataList); //Guarda los datos
+        setTotalPage(response.data.totalPage);//Total de Paginas
+        setCurrentPage(response.data.currentPage)//Actualiza la pagina en donde estan los datos
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+     
+}
   let items = [];
  
-  for (let number = 0; number <= totalPage; number++) {
+  for (let number = 0; number < totalPage; number++) {
     items.push(
       <Pagination.Item key={number} >
         {number}
@@ -46,7 +67,11 @@ const ListarColegios = () => {
     );
   }
 
-  
+  const handleCurrentPage=(idPage)=>{
+    setCurrentPage(idPage);
+    recargaDatos(idPage);
+    
+}
     
   
   return (
@@ -60,27 +85,32 @@ const ListarColegios = () => {
           <Table bordered >
             <thead>
               <tr>
-
+              <th id="table-border">Numero</th>
                 <th id="table-border">Nombre</th>
                 <th id="table-border">Administrador</th>
+                
                 <th className="actions-th" id="table-border">Acciones</th>
               </tr>
             </thead>
             <tbody >
               {colegios.map((colegio, index) => (
                 <tr key={colegio.id}>
+                  <td id="table-border" className="numero-td" >{index+1}</td>
                   <td id="table-border" >{colegio.nombre}</td>
                   <td id="table-border">{colegio.nombreAdministrador} {colegio.apellido}</td>
                   <td className="actions-td" id="table-border"><button className="information-buttons"><BiTrash /></button> <button className="information-buttons"><BiPencil /> </button> <button className="information-buttons"><BiInfoCircle /></button></td>
                 </tr>
+                
               ))}
+              
+               
             </tbody>
           </Table>
         </div>
-        <div className="div-button-agregar">
-          <div>
-            <Pagination onClick={()=>setCurrentPage(1)} className="paginacion" size="sm">{items} </Pagination>
-          </div>
+        <div className="paginacion">
+          
+            <Pagination onClick={e => handleCurrentPage(e.target.text)}  size="sm">{items} </Pagination>
+          
         </div>
         <ModalAgregarColegios></ModalAgregarColegios>
       </div>
--- profefolio-frontend/src/pages/administradorMaster/colegios/ModalAgregarColegios.jsx
+++ profefolio-frontend/src/pages/administradorMaster/colegios/ModalAgregarColegios.jsx
@@ -3,6 +3,7 @@ import { Modal, Button } from 'react-bootstrap'
 import "./ModalAgregarColegios.css"
 import { BsFillPlusCircleFill } from "react-icons/bs"
 import axios from "axios";
+import Pagination from 'react-bootstrap/Pagination';
 function ModalDialog() {
     const [nombreColegio, setNombreColegio] = useState(null);
     const [idAdmin, setIdAdmin] = useState(0);
@@ -13,7 +14,7 @@ function ModalDialog() {
         method: 'get',
         url: 'https://localhost:7063/api/administrador/page/0',
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQ2FybG9zLlRvcnJlczEyM0BtYWlsLmNvbSIsImp0aSI6IjY3NjdkMTZhLTQxNWItNGRhZC1iMTRjLTBjZjZkNDI2ZDRmYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hc3RlciIsImV4cCI6MTY3ODg1MDUwOSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.R_dHv3w5gutvZzBcO2x2px8RyqIdVbgrlL2eck-fXAM',
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQ2FybG9zLlRvcnJlczEyM0BtYWlsLmNvbSIsImp0aSI6ImFhZDVlZjAzLTU5YTMtNDFkNy04YTk3LWQ4NDM5YjE2NmI4OSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hc3RlciIsImV4cCI6MTY3ODkwNjQzMCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.s_2BhsMsPThYPoK7H8KdsjoH8mZcOCLGozlr7CC9hFU',
             'Content-Type': 'application/json'
         },
 
@@ -22,22 +23,23 @@ function ModalDialog() {
     useEffect(() => {
         axios(config)
             .then(function (response) {
-                console.log(JSON.stringify(response.data));
+                
                 setAdministradores(response.data.dataList);
             })
             .catch(function (error) {
                 console.log(error);
             });
     }, [])
+    const handleAdmin=(idAdmin)=>{
+        console.log(idAdmin);
+        setIdAdmin(idAdmin);
+    }
     const handleSubmit = (event) => {
-
-        event.preventDefault();
-    
-        var axios = require('axios');
+        //event.preventDefault();
+        
         var data = JSON.stringify({
-          "nombre": JSON.stringify(nombreColegio),
-          "estado":"true",
-          "personaId": JSON.stringify(idAdmin)
+          "nombre": nombreColegio,
+          "personaId": idAdmin
         });
         
         var config = {
@@ -45,7 +47,7 @@ function ModalDialog() {
         maxBodyLength: Infinity,
           url: 'https://localhost:7063/api/Colegios',
           headers: { 
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQ2FybG9zLlRvcnJlczEyM0BtYWlsLmNvbSIsImp0aSI6IjFiOWRjZTExLWNlZjQtNGI0Ny05MTRhLThjOWM0OWM0Nzc3NiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hc3RlciIsImV4cCI6MTY3ODc3MzgwOCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.TBGSrTUNbwoZEWXR4yd4uWR3W6Y_bgr9F3XCOIycHUI', 
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiQ2FybG9zLlRvcnJlczEyM0BtYWlsLmNvbSIsImp0aSI6ImFhZDVlZjAzLTU5YTMtNDFkNy04YTk3LWQ4NDM5YjE2NmI4OSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hc3RlciIsImV4cCI6MTY3ODkwNjQzMCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo0MjAwIn0.s_2BhsMsPThYPoK7H8KdsjoH8mZcOCLGozlr7CC9hFU', 
             'Content-Type': 'application/json'
           },
           data : data
@@ -53,13 +55,28 @@ function ModalDialog() {
         
         axios(config)
         .then(function (response) {
-          console.log(JSON.stringify(response.data));
+            if(response.status>=400){
+                console.log("Hubo un error")
+            }
+          else if(response.status>=200){
+            console.log("Guardado correctamente");
+          }
         })
         .catch(function (error) {
           console.log(error);
         });
+        closeModal();
         
 }
+let items = [];
+ 
+  for (let number = 0; number < 2; number++) {
+    items.push(
+      <Pagination.Item key={number} >
+        {number}
+      </Pagination.Item>,
+    );
+  }
 
     const [isShow, invokeModal] = React.useState(false)
     const initModal = () => {
@@ -82,7 +99,7 @@ function ModalDialog() {
                             <input required type="text" id="input-colegio" name="colegio-nombre" onChange={event => setNombreColegio(event.target.value)}></input><br />
                             <label for="administrador"><strong> Administrador</strong></label><br />
 
-                            <select name="admin" value={idAdmin} onChange={e => setIdAdmin(e.target.value)}>
+                            <select name="admin" value={idAdmin} onChange={e => handleAdmin(e.target.value)} className="select_admin">
                                 {administradores.map((administrador) =>
                                     <option key={administrador.id} value={administrador.id}>{administrador.nombre} {administrador.apellido}</option>
                                 )}
@@ -95,7 +112,7 @@ function ModalDialog() {
                     <Button className="btn btn-light btn-sm" onClick={closeModal}>
                         Cancelar
                     </Button>
-                    <Button className='btn-guardar btn-sm' type='submit'>
+                    <Button className='btn-guardar btn-sm' onClick={()=>handleSubmit()}>
                         Guardar
                     </Button>
                 </Modal.Footer>
