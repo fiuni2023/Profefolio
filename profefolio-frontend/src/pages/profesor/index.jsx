65ab47da1dbb266fb52a08ec44554660916895c8
KaitoK1kuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-03-16 04:47:54 -0300

    Commit fixes for Profesores

--- profefolio-frontend/src/pages/profesor/index.jsx
+++ profefolio-frontend/src/pages/profesor/index.jsx
@@ -10,143 +10,146 @@ import axios from 'axios';
 import "./components/create/Index.module.css";
 
 
-import {BsTrash , BsPencilFill, BsInfoCircle,BsFillPlusCircleFill} from 'react-icons/bs';
+import { BsTrash, BsPencilFill, BsInfoCircle, BsFillPlusCircleFill } from 'react-icons/bs';
+import APILINK from '../../components/link.js';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router';
 
 
 function Profesores() {
-    const [profesores, setProfesores] = useState([]);
-    const [page, setPage] = useState(0);
-    const [size, setSize] = useState(10);
-
-    
-    const { getToken } = useGeneralContext();
+  const [profesores, setProfesores] = useState([]);
+  const [page, setPage] = useState(0);
 
 
+  const { getToken, cancan, verifyToken } = useGeneralContext();
 
-    useEffect(() => {
+  const nav = useNavigate()
 
-        //axios.get(`https://miapi.com/products?page=${page}&size=${size}`, {
-
-      axios.get(`https://localhost:7063/api/profesor/page/${page}`, {
+  useEffect(() => {
+    verifyToken()
+    if(!cancan("Administrador de Colegio")){
+      nav("/")
+    }else{
+      //axios.get(`https://miapi.com/products?page=${page}&size=${size}`, {
+  
+      axios.get(`${APILINK}/api/profesor/page/${page}`, {
         headers: {
-         Authorization:  `Bearer ${getToken()}`,
+          Authorization: `Bearer ${getToken()}`,
         }
-        })
-
-      .then(response => {
-        setProfesores(response.data.dataList);
-        console.log(JSON.stringify(response.data.dataList))
-
       })
-      .catch(error => {
-        console.error(error);
-      });
-    },[page, size]);
-
-
-
-
   
-    const handlePrevClick = () => {
-      setPage(page - 1);
-    };
+        .then(response => {
+          setProfesores(response.data.dataList);
   
-    const handleNextClick = () => {
-      setPage(page + 1);
-    };
-
-    
-
-    const [show, setShow] = useState(false);
-
-    
-    return(      
-        <>
-    
-            <div className="page">
-            <NavAdmin/>
-           
-            <PanelContainerBG>
-          
-
-                        <div>
-                            <table className="CustomTable">
-                                <thead>
-                                <tr>
-                                    <th>CI</th>
-                                    <th>Nombre</th>
-                                    <th>Apellido</th>
-                                    <th>Fecha de nacimiento</th>
-                                    <th>Direccion</th>
-                                    <th>Telefono</th>
-                                    <th>Acciones</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    
-                                 {profesores.map(profe => (
-                                    <tr key={profe.id}>
-                                    <td>{profe.documento}</td>
-                                    <td>{profe.nombre}</td>
-                                    <td>{profe.apellido}</td>
-                                    <td>{profe.nacimiento}</td>
-                                    <td>{profe.direccion}</td>
-                                    <td>{profe.telefono}</td>
-                                    <td> <BsTrash/>  <BsPencilFill/> <BsInfoCircle/></td>
-
-                                    </tr>
-                                ))}
-                                </tbody>
-                            </table>
-                           
-
-                            <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-end">
-                                  <li class="page-item disabled">
-                                    <button class="pag" onClick={handlePrevClick} disabled={page === 0}>Anterior</button>
-                                  </li>
-                                  <li class="page-item">
-                                    <button class="pag" href="#" onClick={handleNextClick}>Siguiente</button>
-                                  </li>
-                                </ul>
-                              </nav>        
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [page, cancan, verifyToken, nav]);
 
 
 
-                            </div>
 
-    
 
-           
-               
+  const handlePrevClick = () => {
+    setPage(page - 1);
+  };
 
-               
-            </PanelContainerBG>
-            <footer>
-             <div className="NButtonForSideA "> 
-             <button className="buttonNavBarAa">  
-            <CreateModal title="My Modal" onClose={() => setShow(false)} show={show}>
+  const handleNextClick = () => {
+    setPage(page + 1);
+  };
+
+
+
+  const [show, setShow] = useState(false);
+
+
+  return (
+    <>
+
+      <div className="page">
+        <NavAdmin />
+
+        <PanelContainerBG>
+
+
+          <div>
+            <table className="CustomTable">
+              <thead>
+                <tr>
+                  <th>CI</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Fecha de nacimiento</th>
+                  <th>Direccion</th>
+                  <th>Telefono</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                {profesores.map(profe => (
+                  <tr key={profe.id}>
+                    <td>{profe.documento}</td>
+                    <td>{profe.nombre}</td>
+                    <td>{profe.apellido}</td>
+                    <td>{profe.nacimiento}</td>
+                    <td>{profe.direccion}</td>
+                    <td>{profe.telefono}</td>
+                    <td> <BsTrash />  <BsPencilFill /> <BsInfoCircle /></td>
+
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+
+            <nav aria-label="Page navigation example">
+              <ul className="pagination justify-content-end">
+                <li className="page-item disabled">
+                  <button className="pag" onClick={handlePrevClick} disabled={page === 0}>Anterior</button>
+                </li>
+                <li className="page-item">
+                  <button className="pag" href="#" onClick={handleNextClick}>Siguiente</button>
+                </li>
+              </ul>
+            </nav>
+
+
+
+          </div>
+
+
+
+
+
+
+
+        </PanelContainerBG>
+        <footer>
+          <div className="NButtonForSideA ">
+            <CreateModal title="My Modal" onClose={() => setShow(false)} onSubmit={(value)=>{setProfesores([...profesores, value])}} show={show}>
             </CreateModal>
+            
+          </div>
 
-            </button>
-            </div>
-
-            </footer>
-
-          
-        
-             
+        </footer>
 
 
 
-           
 
 
-           
 
-            </div>
 
-            <style jsx='true'>{`
+
+
+
+
+
+      </div>
+
+      <style jsx='true'>{`
             .page{
                 display: grid;
                 grid-template-rows: 5% 95%;
@@ -166,7 +169,10 @@ function Profesores() {
                 background-color: #F0544F;
             }
             .NButtonForSideA{
-               
+               width: 25%;
+               position: absolute;
+               bottom: 5px;
+               right : 5px;
             }
             .buttonNavBarA{
                 width: 100%;
@@ -213,8 +219,8 @@ function Profesores() {
 
             
             `}</style>
-        </>
-    )
+    </>
+  )
 }
 
 export default Profesores
