353e3b032dc3dfefb149ddec604b52b72b0f2050
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-19 00:12:26 -0300

    Se creo el modelo de Clase

--- profefolio/Models/Entities/Clase.cs
+++ profefolio/Models/Entities/Clase.cs
@@ -1,5 +1,7 @@
+using System.Runtime.CompilerServices;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,23 @@ namespace profefolio.Models.Entities
 {
     public class Clase : Data
     {
-        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage ="Se tiene que indicar el Colegio")]
+        public int IdColegio { get; set; }
+
+        [Required(ErrorMessage ="Se tiene que indicar el Ciclo")]
+        public int IdCiclo { get; set; }
+
+        [Required(ErrorMessage = "Se tiene que agregar un nombre a la clase")]
+        [MaxLength(128)]
+        public string? Nombre { get; set; }
+        
+        [Required(ErrorMessage = "Se tiene que agregar el turno")]
+        [MaxLength(32)]
+        public string? Turno { get; set; }
+
+        [Required(ErrorMessage = "Se tiene que agregar el anho")]
+        public int Anho { get; set; }
+
     }
 }
