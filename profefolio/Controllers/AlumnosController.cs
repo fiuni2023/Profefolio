8dc21f066d2db255cef2292ca7577de49f151c03
Carlos Torres <cctc2013@gmail.com>
2023-03-18 21:50:12 -0300

    Creado Alumnos Controller

---
+++ profefolio/Controllers/AlumnosController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using profefolio.Repository;
+
+namespace profefolio.Controllers;
+
+[Authorize(Roles = "Administrador de Colegio")]
+[Route("[controller]")]
+public class AlumnosController : ControllerBase
+{
+    private readonly IPersona _personaService;
+    private readonly IMapper _mapper;
+
+    public AlumnosController(IPersona personaService, IMapper mapper)
+    {
+        _personaService = personaService;
+        _mapper = mapper;
+    }
+}
