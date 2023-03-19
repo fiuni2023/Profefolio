8153d91636131013934ced7cfbf98927b9d1e0a1
Carlos Torres <cctc2013@gmail.com>
2023-03-18 22:07:48 -0300

    Agregado el Mapper entre Alumno y la Base de Datos

---
+++ profefolio/Helpers/Mappers/AlumnoMapper.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using profefolio.Models.DTOs.Alumno;
+using profefolio.Models.DTOs.Persona;
+using profefolio.Models.Entities;
+
+namespace profefolio.Helpers.Mappers;
+
+public class AlumnoMapper : Profile
+{
+    public AlumnoMapper()
+    {
+        CreateMap<AlumnoCreateDTO, Persona>()
+            .ForMember(dest => dest.Id,
+                opt => opt.Ignore())
+            .ForMember(dest => dest.Created,
+                opt => opt.MapFrom(
+                    src => DateTime.Now))
+            .ForMember(dest => dest.Modified,
+                opt => opt.Ignore())
+            .ForMember(dest => dest.ModifiedBy,
+                opt => opt.Ignore())
+            .ForMember(dest => dest.EsM,
+                opt => opt.MapFrom(
+                    src => src.Genero != null && src.Genero.Equals("M")
+                ))
+            .ForMember(dest => dest.SecurityStamp,
+                opt => opt.MapFrom(
+                    t => Guid.NewGuid().ToString()));
+
+
+        CreateMap<Persona, AlumnoCreateDTO>()
+            .ForMember(dest => dest.Genero,
+                opt => opt.MapFrom(
+                    src => src.EsM ? "Masculino" : "Femenino"));
+    }
+}
--- profefolio/Models/DTOs/Persona/PersonaDTO.cs
+++ profefolio/Models/DTOs/Persona/PersonaDTO.cs
@@ -1,25 +1,89 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace profefolio.Models.DTOs.Persona;
+// ReSharper disable once InconsistentNaming
 public class PersonaDTO
 {
     [Required(ErrorMessage = "El nombre es requerido")]
-    public string Nombre { get; set; }
+    public string? Nombre
+    {
+        get;
+        set;
+    }
 
     [Required(ErrorMessage = "El apellido es requerido")]
-    public string Apellido { get; set; }
+    public string? Apellido
+    {
+        get;
+        set;
+    }
+
     
     [Required(ErrorMessage = "El Nacimiento es requerido")]
-    public DateTime Nacimiento { get; set; }
+    public DateTime Nacimiento
+    {
+        get;
+        set;
+    }
 
     [Required(ErrorMessage = "El Documento es requerido")]
-    public string Documento { get; set; }
+    public string? Documento
+    {
+        get;
+        set;
+    }
 
     [Required(ErrorMessage = "El Tipo de Documento es requerido")]
-    public string DocumentoTipo { get; set; }
+    public string? DocumentoTipo
+    {
+        get;
+        set;
+    }
 
     [Required(ErrorMessage = "El genero es requerido")]
-    public string Genero { get; set; }
-    public string Direccion { get; set; }
+    public string? Genero
+    {
+        get;
+        set;
+    }
 
+   
+    public string? Direccion
+    {
+        get;
+        set;
+    }
+
+    [Required(ErrorMessage = "El telefono es requerido")]
+    public string? Telefono
+    {
+        get;
+        set;
+    }
+    
+    [RegularExpression(@"^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$", 
+        ErrorMessage = "Se requiere como minimo: longitud de 8, letras mayusculas y minusculas, numero y caracter no Alfanumerico ")]
+
+    [Required(ErrorMessage = "la contraseña es requerida")]
+    public string? Password
+    {
+        get;
+        set;
+    }
+
+    [Required(ErrorMessage = "necesitas confirmar la contraseña")]
+    [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
+    public string? ConfirmPassword
+    {
+        get;
+        set;
+    }
+
+    [Required(ErrorMessage = "El Email es requerido")]
+    [EmailAddress(ErrorMessage = "El email debe ser de formato Mail")]
+    public string? Email
+    {
+        get;
+        set;
+    }
 }
