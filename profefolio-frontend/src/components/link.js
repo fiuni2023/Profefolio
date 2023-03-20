e8cacae4055f691f904956962d1b065634426524
dahybarboza <126834904+dahybarboza@users.noreply.github.com>
2023-03-19 22:20:59 -0300

    correccion de link

--- profefolio-frontend/src/components/link.js
+++ profefolio-frontend/src/components/link.js
@@ -1,6 +1,5 @@
-//const APILINK = 'https://profefolio.azurewebsites.net'
+const APILINK = 'https://profefolio.azurewebsites.net'
 
-const APILINK = 'https://localhost:7063'
 
 
 export default APILINK
