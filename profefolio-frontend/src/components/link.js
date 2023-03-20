63868d60057bcbe49ab9f011ec9c73f9e58214fc
dahybarboza <126834904+dahybarboza@users.noreply.github.com>
2023-03-19 23:58:54 -0300

    conflictos resueltos

--- profefolio-frontend/src/components/link.js
+++ profefolio-frontend/src/components/link.js
@@ -1,4 +1,6 @@
-const APILINK = 'https://profefolio.azurewebsites.net/'
+//const APILINK = 'https://profefolio.azurewebsites.net/'
+
+const APILINK = 'https://localhost:7063'
 
 
 export default APILINK
