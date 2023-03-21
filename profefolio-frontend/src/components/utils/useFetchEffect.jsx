776efceafdea1bb6f300f67a83d1db3b104ed02a
KaitoK1kuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-03-20 23:39:37 -0300

    Cambios en el modal create y utilizacion del useFetch Effect

--- profefolio-frontend/src/components/utils/useFetchEffect.jsx
+++ profefolio-frontend/src/components/utils/useFetchEffect.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 
-export const useFetchEffect = ( asyncFunc = async() => {}, dependences = [], settings = {condition:() => {return true}, handleSuccess:()=>{}, handleError: ()=>{}} ) => {
+export const useFetchEffect = (asyncFunc = async () => { }, dependences = [], settings = { condition: false, handleSuccess: () => { }, handleError: () => { } }) => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
     const [toggle, toggleFetch] = useState([])
+
 
     const doFetch = () => {
         let newFetch = toggle
@@ -11,20 +12,29 @@ export const useFetchEffect = ( asyncFunc = async() => {}, dependences = [], set
         toggleFetch(newFetch)
     }
 
-    useEffect(()=>{
+
+    useEffect(() => {
+
+        let seguir = settings.condition
+        if(!seguir) return;
+
         setLoading(true)
-        if(settings.condition){
-            asyncFunc()
-            .then((result)=>{
+        asyncFunc()
+            .then((result) => {
+                console.log(result)
                 setLoading(false)
                 settings.handleSuccess(result)
             })
-            .catch((error)=>{
+            .catch((error) => {
                 setError(true)
                 settings.handleError(error)
             })
-        }
-    }, [ dependences, settings, asyncFunc])
+            .finally(() => {
+                setLoading(false)
+            })
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, dependences)
 
     return [loading, error, doFetch]
 }
--- profefolio-frontend/src/pages/administrador/pages/list/index.jsx
+++ profefolio-frontend/src/pages/administrador/pages/list/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./index.module.css"
 import { HiArrowLeft } from 'react-icons/hi'
 import { AiOutlinePlus } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
 import { Table } from "../../../../components/Table";
 import LACreateModal from "../../components/CreateModal";
 import { useGeneralContext } from "../../../../context/GeneralContext";
@@ -9,45 +10,50 @@ import AdminService from "../../servicios/Administradores";
 import { Pagination } from "react-bootstrap";
 
 import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router";
+import { useFetchEffect } from "../../../../components/utils/useFetchEffect";
+import { useNavigate } from "react-router-dom";
 
 const ListAdministrador = () => {
-
-    const nav = useNavigate()
-    const {getToken, verifyToken, cancan} = useGeneralContext()
+    const {getToken, cancan, verifyToken} = useGeneralContext()
 
     const [showCreateModal, setShowCreateModal] = useState(false)
     const [admins, setAdmins] = useState([])
     const [currentPage, setCurrentPage] = useState(0)
     const [next, setNext] = useState(true)
-    const [loading, setLoading] = useState(false)
+    const [condFetch, setCondFetch] = useState(false)
 
     const parseToDate = (d=new Date()) => {
         return `${d.getFullYear()}-${d.getMonth()>10? d.getMonth():`0${d.getMonth()}`}-${d.getDate()>10? d.getDate():`0${d.getDate()}`}`
     }
 
+    const nav = useNavigate()
+
     useEffect(()=>{
         verifyToken()
         if(!cancan("Master")){
-            nav("/")
+            nav('/')
         }else{
-            setLoading(true)
-            AdminService.getList(currentPage, getToken())
-            .then(r=>{
+            setCondFetch(true)
+        }
+    }, [cancan, verifyToken, nav])
+    
+
+    const [isLoading, error, doFetch] = useFetchEffect(
+        ()=>{
+            return AdminService.getList(currentPage, getToken())
+        }, 
+        [currentPage, getToken],
+        {
+            condition: condFetch,
+            handleSuccess: (r)=>{
                 setAdmins(r.data.dataList)
                 setNext(r.data.next)
-                setLoading(false)
-            })
-            .catch(e=>{
+            },
+            handleError: ()=>{
                 toast.error("No se pudieron cargar los administradores, intente de nuevo")
-                setLoading(false)
-            })
+            }
         }
-    }, [currentPage, getToken, cancan, verifyToken, nav])
-
-    const doFetch =(admin) =>{
-        setAdmins([...admins, admin])
-    }
+    )
 
     const getPages = () => {
         return (
@@ -56,17 +62,16 @@ const ListAdministrador = () => {
                     setCurrentPage(currentPage-1)
                 }} />
                 <Pagination.Item disabled >{currentPage + 1}</Pagination.Item>
-                <Pagination.Next disabled={!next || loading} onClick={()=>{
+                <Pagination.Next disabled={!next || isLoading || error} onClick={()=>{
                     setCurrentPage(currentPage+1)
                 }}/>
             </>
         )
     }
 
-
     return (
         <>
-            <LACreateModal show={showCreateModal} handleClose={()=>{setShowCreateModal(!showCreateModal)}} triggerState={(admin)=>{doFetch(admin)}} />
+            <LACreateModal show={showCreateModal} handleClose={()=>{setShowCreateModal(!showCreateModal) }} triggerState={()=>{doFetch()}} />
             <div className={styles.GridContainer}>
                 <div className={styles.LANavbar}> 
                     <HiArrowLeft size={"20px"} onClick={()=>nav("/")}/>
@@ -80,11 +85,11 @@ const ListAdministrador = () => {
                             return(
                                 <tr key={index}>
                                     <td>{d?.documento}</td>
-                                    <td>{d?.nombre + " " + d?.apellido}</td>
+                                    <td>{`${d?.nombre} ${d?.apellido}`}</td>
                                     <td>{parseToDate(new Date(d?.nacimiento))}</td>
                                     <td>{d?.direccion}</td>
                                     <td>{d?.telefono}</td>
-                                    <td></td>
+                                    <td><BsInfoCircle size={10} onClick={()=>{toast.success(`Administrador con id ${d?.id}`)}} style={{cursor:"pointer"}} /></td>
                                 </tr>
                             )
                         }}
