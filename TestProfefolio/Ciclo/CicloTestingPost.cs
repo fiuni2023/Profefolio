144bebe0b1822f1761605e00744a2866d2ff6fd3
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-23 18:10:24 -0300

    Se agrego el test para el caso en el que suceda un error durante el guardado el nuevo Ciclo

--- TestProfefolio/Ciclo/CicloTestingPost.cs
+++ TestProfefolio/Ciclo/CicloTestingPost.cs
@@ -19,6 +19,9 @@ namespace TestProfefolio.Ciclo;
 public class CicloPostOk
 {
 
+    /*
+        Testea cuando se guarda exitosamente el nuevo Ciclo
+    */
     [Fact]
     public async void Post_Ok()
     {
@@ -50,7 +53,7 @@ public class CicloPostOk
         var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
             //new Claim(ClaimTypes.NameIdentifier, "user1") -- usar esta linea si es que funciona el id del usuario autorizado
-            new Claim(ClaimTypes.Name, "user1") 
+            new Claim(ClaimTypes.Name, "user1")
         }, "mock"));
 
         controller.ControllerContext = new ControllerContext()
@@ -64,19 +67,22 @@ public class CicloPostOk
 
         service.Setup(s => s.Add(modelo)).ReturnsAsync(modelo);
 
-        service.Setup(b => b.Save());
+        service.Setup(a => a.Save()).Returns(Task.CompletedTask);
 
         mapper.Setup(m => m.Map<CicloResultDTO>(modelo)).Returns(resultDto);
 
-        service.Setup(a => a.Save()).Returns(Task.CompletedTask);
         var result = await controller.Post(dto);
-
 
         Assert.IsType<OkObjectResult>(result.Result);
     }
 
+
+    /*
+        Testea cuando se pasa un modelo invalido al metodo de Post
+    */
     [Fact]
-    public async void Post_ModelInvalid_BadRequest(){
+    public async void Post_ModelInvalid_BadRequest()
+    {
 
         Mock<IMapper> mapper = new Mock<IMapper>();
         Mock<ICiclo> service = new Mock<ICiclo>();
@@ -106,7 +112,7 @@ public class CicloPostOk
         var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
             //new Claim(ClaimTypes.NameIdentifier, "user1") -- usar esta linea si es que funciona el id del usuario autorizado
-            new Claim(ClaimTypes.Name, "user1") 
+            new Claim(ClaimTypes.Name, "user1")
         }, "mock"));
 
         controller.ControllerContext = new ControllerContext()
@@ -120,15 +126,19 @@ public class CicloPostOk
 
         var result = await controller.Post(dto);
         BadRequestObjectResult r = (BadRequestObjectResult)result.Result;
-        
-        
+
+
         Assert.Equal("Peticion invalido", r.Value.ToString());
         Assert.IsType<BadRequestObjectResult>(result.Result);
     }
 
+    /*
+        Testea cuando se quiere agregar un nuevo Ciclo con un nombre ya existente
+    */
     [Fact]
-    public async void Post_ExistOtherWithEqualName_BadRequest(){
-                Mock<IMapper> mapper = new Mock<IMapper>();
+    public async void Post_ExistOtherWithEqualName_BadRequest()
+    {
+        Mock<IMapper> mapper = new Mock<IMapper>();
         Mock<ICiclo> service = new Mock<ICiclo>();
         CicloController controller = new CicloController(mapper.Object, service.Object);
 
@@ -155,7 +165,7 @@ public class CicloPostOk
 
         var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
-            new Claim(ClaimTypes.Name, "user1") 
+            new Claim(ClaimTypes.Name, "user1")
         }, "mock"));
 
         controller.ControllerContext = new ControllerContext()
@@ -167,9 +177,69 @@ public class CicloPostOk
 
         var result = await controller.Post(dto);
         BadRequestObjectResult r = (BadRequestObjectResult)result.Result;
-        
-        
+
+
         Assert.Equal("Ya existe un Ciclo con ese nombre", r.Value.ToString());
+        Assert.IsType<BadRequestObjectResult>(result.Result);
+    }
+
+
+    /*
+        Testea cuando sucede un error durante el guardado del Ciclo
+    */
+    [Fact]
+    public async void Post_ErrorSave_BadRequest()
+    {
+        Mock<IMapper> mapper = new Mock<IMapper>();
+        Mock<ICiclo> service = new Mock<ICiclo>();
+        CicloController controller = new CicloController(mapper.Object, service.Object);
+
+
+        CicloDTO dto = new CicloDTO()
+        {
+            Nombre = "Primero"
+        };
+
+        profefolio.Models.Entities.Ciclo modelo = new profefolio.Models.Entities.Ciclo()
+        {
+            Id = 1,
+            Nombre = "Primero",
+            Created = DateTime.Now,
+            Deleted = false,
+            CreatedBy = "123456"
+        };
+
+        CicloResultDTO resultDto = new CicloResultDTO()
+        {
+            Id = 1,
+            Nombre = "Primero"
+        };
+
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.Name, "user1")
+        }, "mock"));
+
+        controller.ControllerContext = new ControllerContext()
+        {
+            HttpContext = new DefaultHttpContext() { User = user }
+        };
+
+        service.Setup(a => a.ExisitNombre(dto.Nombre)).ReturnsAsync(false);
+
+        mapper.Setup(m => m.Map<profefolio.Models.Entities.Ciclo>(dto)).Returns(modelo);
+
+        service.Setup(s => s.Add(modelo));//.ReturnsAsync(modelo);
+
+        service.Setup(a => a.Save()).ThrowsAsync(new Exception("Error al guardar"));
+
+        mapper.Setup(m => m.Map<CicloResultDTO>(modelo)).Returns(resultDto);
+
+        var result = await controller.Post(dto);
+
+        BadRequestObjectResult r = (BadRequestObjectResult)result.Result;
+
+        Assert.Equal("Error durante el guardado.", r.Value.ToString());
         Assert.IsType<BadRequestObjectResult>(result.Result);
     }
 }
