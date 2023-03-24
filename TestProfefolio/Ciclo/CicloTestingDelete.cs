58c737b804a4fddcecf2ff2711888203fa1c01e7
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-24 00:25:17 -0300

    Se creo el test para poder testear un caso en el que no es encontrado el id del Ciclo a eliminar

--- TestProfefolio/Ciclo/CicloTestingDelete.cs
+++ TestProfefolio/Ciclo/CicloTestingDelete.cs
@@ -61,5 +61,42 @@ namespace TestProfefolio.Ciclo
             Assert.IsType<OkResult>(result);
         }
 
+
+
+        /*
+            Testea cuando se elimina exitosamente un Ciclo
+        */
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public async void Delete_NoFindId_BadRequest(int id)
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> service = new Mock<ICiclo>();
+            CicloController controller = new CicloController(mapper.Object, service.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "user1")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+
+            service.Setup(c => c.FindById(id));
+
+            var result = await controller.Delete(id);
+
+            BadRequestObjectResult r = (BadRequestObjectResult)result;
+
+            Assert.Equal("Ciclo no encontrado", r.Value.ToString());       
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
     }
 }
