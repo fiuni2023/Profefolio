a53f56a748b8842d7a483a2b0ee259c81e55fce7
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-25 18:32:17 -0300

    Se agrego un test para el caso de fallo cuando se manda un anho menor o igual que 1950 en el DTO

--- TestProfefolio/Clase/ClaseTestingPut.cs
+++ TestProfefolio/Clase/ClaseTestingPut.cs
@@ -166,5 +166,95 @@ namespace TestProfefolio.Clase
             var response = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("Peticion Invalida", response.Value);
         }
+
+
+
+        /*
+            Testea un caso de fallo en el que el anho mandado en el DTO es menor o igual a 1950
+        */
+        [Theory]
+        [InlineData(1, 1800)]
+        [InlineData(2, 1950)]
+        [InlineData(3, 1949)]
+        [InlineData(4, 1941)]
+        [InlineData(5, 1910)]
+        [InlineData(6, 0)]
+        [InlineData(7, 2)]
+        [InlineData(8, 1200)]
+        public async void Put_InvalidYear_BadRequest(int id, int anho)
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> cicloService = new Mock<ICiclo>();
+            Mock<IClase> claseService = new Mock<IClase>();
+            Mock<IColegio> colegioService = new Mock<IColegio>();
+
+            ClaseController controller = new ClaseController(
+                mapper.Object,
+                claseService.Object,
+                cicloService.Object,
+                colegioService.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "user1")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var dto = new ClaseDTO()
+            {
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Anho = anho,
+                CicloId = 1,
+                ColegioId = 1
+            };
+
+            var clase = new profefolio.Models.Entities.Clase()
+            {
+                Anho = anho,
+                CicloId = 1,
+                ColegioId = 1,
+                Deleted = false,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Created = DateTime.Now,
+                CreatedBy = "juan.perez@gmail.com"
+            };
+
+            var claseResult = new ClaseResultDTO()
+            {
+                Id = id,
+                Anho = anho,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                IdCiclo = 1,
+                IdColegio = 1,
+                Ciclo = "Primero",
+                Colegio = "San Juan"
+            };
+
+            var ciclo = new profefolio.Models.Entities.Ciclo()
+            {
+                Id = 1,
+                Nombre = "Primero"
+            };
+
+            var colegio = new profefolio.Models.Entities.Colegio()
+            {
+                Id = 1,
+                Nombre = "San Juan",
+                PersonaId = "123456789"
+            };
+
+            var result = await controller.Put(id, dto);
+
+            var response = Assert.IsType<BadRequestObjectResult>(result);
+
+            Assert.Equal("Anho invalido", response.Value);
+        }
     }
 }
