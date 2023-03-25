f577eb27f9a2ef85cb119b7a2eee6292f47f417e
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-25 18:47:35 -0300

    Se agrego un test para el caso de fallo cuando no se encuentra un Ciclo con el ID recibido en el DTO

--- TestProfefolio/Clase/ClaseTestingPut.cs
+++ TestProfefolio/Clase/ClaseTestingPut.cs
@@ -271,7 +271,7 @@ namespace TestProfefolio.Clase
         [InlineData(6)]
         [InlineData(7)]
         [InlineData(8)]
-        public async void Put_IdClaseNotFound_BadRequest(int id)
+        public async void Put_IdClaseNotFound_NotFound(int id)
         {
             Mock<IMapper> mapper = new Mock<IMapper>();
             Mock<ICiclo> cicloService = new Mock<ICiclo>();
@@ -348,6 +348,102 @@ namespace TestProfefolio.Clase
             var response = Assert.IsType<NotFoundObjectResult>(result);
         
             Assert.Equal("No se ha encontrado la Clase a editar", response.Value);
+        }
+    
+    
+    
+    
+        /*
+            Testea un caso de fallo cuando no se encuentra un Ciclo con el Id que se recibio en el DTO
+        */
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        public async void Put_IdCicloNotFound_NotFound(int id)
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> cicloService = new Mock<ICiclo>();
+            Mock<IClase> claseService = new Mock<IClase>();
+            Mock<IColegio> colegioService = new Mock<IColegio>();
+
+            ClaseController controller = new ClaseController(
+                mapper.Object,
+                claseService.Object,
+                cicloService.Object,
+                colegioService.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "user1")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var dto = new ClaseDTO()
+            {
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1
+            };
+
+            var clase = new profefolio.Models.Entities.Clase()
+            {
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1,
+                Deleted = false,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Created = DateTime.Now,
+                CreatedBy = "juan.perez@gmail.com"
+            };
+
+            var claseResult = new ClaseResultDTO()
+            {
+                Id = id,
+                Anho = 2023,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                IdCiclo = 1,
+                IdColegio = 1,
+                Ciclo = "Primero",
+                Colegio = "San Juan"
+            };
+
+            var ciclo = new profefolio.Models.Entities.Ciclo()
+            {
+                Id = 1,
+                Nombre = "Primero"
+            };
+
+            var colegio = new profefolio.Models.Entities.Colegio()
+            {
+                Id = 1,
+                Nombre = "San Juan",
+                PersonaId = "123456789"
+            };
+
+            claseService.Setup(c => c.FindById(id)).ReturnsAsync(clase);
+
+            cicloService.Setup(c => c.FindById(dto.CicloId));
+
+
+            var result = await controller.Put(id, dto);
+
+            var response = Assert.IsType<NotFoundObjectResult>(result);
+        
+            Assert.Equal("El campo de Ciclo es invalido", response.Value);
         }
     
     }
