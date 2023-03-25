076409c678c4dfb628895ef18b1653db8db7eea9
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-25 19:02:03 -0300

    Se agrego un test para la edicion con un caso de fallo al momento de guardar los cambios

--- TestProfefolio/Clase/ClaseTestingPut.cs
+++ TestProfefolio/Clase/ClaseTestingPut.cs
@@ -544,5 +544,105 @@ namespace TestProfefolio.Clase
             Assert.Equal("El campo de Colegio es invalido", response.Value);
         }
     
+
+
+
+        /*
+            Testea un caso de fallo sucede un error durante el guardado
+        */
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        public async void Put_SaveFailed_BadRequest(int id)
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> cicloService = new Mock<ICiclo>();
+            Mock<IClase> claseService = new Mock<IClase>();
+            Mock<IColegio> colegioService = new Mock<IColegio>();
+
+            ClaseController controller = new ClaseController(
+                mapper.Object,
+                claseService.Object,
+                cicloService.Object,
+                colegioService.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "user1")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var dto = new ClaseDTO()
+            {
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1
+            };
+
+            var clase = new profefolio.Models.Entities.Clase()
+            {
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1,
+                Deleted = false,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Created = DateTime.Now,
+                CreatedBy = "juan.perez@gmail.com"
+            };
+
+            var claseResult = new ClaseResultDTO()
+            {
+                Id = id,
+                Anho = 2023,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                IdCiclo = 1,
+                IdColegio = 1,
+                Ciclo = "Primero",
+                Colegio = "San Juan"
+            };
+
+            var ciclo = new profefolio.Models.Entities.Ciclo()
+            {
+                Id = 1,
+                Nombre = "Primero"
+            };
+
+            var colegio = new profefolio.Models.Entities.Colegio()
+            {
+                Id = 1,
+                Nombre = "San Juan",
+                PersonaId = "123456789"
+            };
+
+            claseService.Setup(c => c.FindById(id)).ReturnsAsync(clase);
+
+            cicloService.Setup(c => c.FindById(dto.CicloId)).ReturnsAsync(ciclo);
+
+            colegioService.Setup(c => c.FindById(dto.ColegioId)).ReturnsAsync(colegio);
+
+            claseService.Setup(c => c.Edit(clase)).Returns(clase);
+
+            claseService.Setup(c => c.Save()).ThrowsAsync(new Exception());
+
+            var result = await controller.Put(id, dto);
+
+            var response = Assert.IsType<BadRequestObjectResult>(result);
+        
+            Assert.Equal("Error durante la edicion", response.Value);
+        }
     }
 }
