09acb5c378beef638bceefae2767e18759a2f6fc
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-24 23:17:29 -0300

    Se agrego un test para el caso de realizar un GetAll sin paginacion con un error durante la obtencion de los Ciclos

--- TestProfefolio/Ciclo/CicloTestingGet.cs
+++ TestProfefolio/Ciclo/CicloTestingGet.cs
@@ -65,5 +65,57 @@ namespace TestProfefolio.Ciclo
         }
 
 
+
+        /*
+            Testea cuando se realiza un GetAll sin paginacion fallida 
+        */
+        [Fact]
+        public async void GetAll_WithoutPage_BadRequest()
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> service = new Mock<ICiclo>();
+            CicloController controller = new CicloController(mapper.Object, service.Object);
+
+
+            CicloResultDTO dto = new CicloResultDTO()
+            {
+                Id = 1,
+                Nombre = "Primero"
+            };
+
+            CicloResultDTO dto2 = new CicloResultDTO()
+            {
+                Id = 2,
+                Nombre = "Segundo"
+            };
+
+            profefolio.Models.Entities.Ciclo modelo = new profefolio.Models.Entities.Ciclo()
+            {
+                Id = 1,
+                Nombre = "Primero",
+                Created = DateTime.Now,
+                Deleted = false,
+                CreatedBy = "123456"
+            };
+            var modelo2 = modelo;
+            modelo2.Id = 2;
+            modelo2.Nombre = "Segundo";
+
+            List<profefolio.Models.Entities.Ciclo> ciclos = new List<profefolio.Models.Entities.Ciclo>(){
+            modelo, modelo2
+        };
+
+            List<CicloResultDTO> resultDto = new List<CicloResultDTO>(){
+            dto, dto2
+        };
+
+            service.Setup(a => a.GetAll()).ThrowsAsync(new Exception("Error durante la busqueda"));
+
+            var result = await controller.GetAll();
+            var r = (BadRequestObjectResult)result.Result;
+
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("Error durante la busqueda", r.Value);
+        }
     }
 }
