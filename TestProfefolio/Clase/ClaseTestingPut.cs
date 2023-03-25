c5f432e3b1f3e492de3fa4926ebe7ef182d20e9c
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-25 18:21:46 -0300

    Se agrego un test para el caso de fallo cuado el modelo de Clase mnadado es invalido

--- TestProfefolio/Clase/ClaseTestingPut.cs
+++ TestProfefolio/Clase/ClaseTestingPut.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using profefolio.Controllers;
 using profefolio.Models.DTOs.Clase;
 using profefolio.Repository;
@@ -108,6 +109,62 @@ namespace TestProfefolio.Clase
             var result = await controller.Put(id, dto);
 
             Assert.IsType<NoContentResult>(result);
+        }
+    
+    
+
+        /*
+            Testea un caso de fallo cuando el modelo es invalido
+        */
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        public async void Put_ModelInvalid_BadRequest(int id)
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> cicloService = new Mock<ICiclo>();
+            Mock<IClase> claseService = new Mock<IClase>();
+            Mock<IColegio> colegioService = new Mock<IColegio>();
+
+            ClaseController controller = new ClaseController(
+                mapper.Object,
+                claseService.Object,
+                cicloService.Object,
+                colegioService.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "user1")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var dto = new ClaseDTO()
+            {
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1
+            };
+
+            var modelState = new ModelStateDictionary();
+            modelState.AddModelError("Nombre", "Propiedad invalida");
+            controller.ModelState.Merge(modelState);
+
+            var result = await controller.Put(id, dto);
+
+            var response = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Peticion Invalida", response.Value);
         }
     }
 }
