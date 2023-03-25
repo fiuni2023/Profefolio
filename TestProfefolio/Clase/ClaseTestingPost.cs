ddc260b1805496149a593bdf0feee63944b4a219
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-25 15:49:43 -0300

    Se agrego un test para el caso de fallo en el que no es encontrado el Id del Colegio mandado en el DTO

--- TestProfefolio/Clase/ClaseTestingPost.cs
+++ TestProfefolio/Clase/ClaseTestingPost.cs
@@ -215,5 +215,72 @@ namespace TestProfefolio.Clase
             Assert.Equal("El campo de Ciclo es invalido", response.Value);
         }
 
+
+        /*
+            Testea un caso de fallo en el que no es encontrado un colegio con el id mandado en el dto
+        */
+        [Fact]
+        public async void Post_IdColegioNotFound_BadRequest()
+        {
+            Mock<IMapper> mapper = new Mock<IMapper>();
+            Mock<ICiclo> cicloService = new Mock<ICiclo>();
+            Mock<IClase> claseService = new Mock<IClase>();
+            Mock<IColegio> colegioService = new Mock<IColegio>();
+
+            ClaseController controller = new ClaseController(
+                mapper.Object,
+                claseService.Object,
+                cicloService.Object,
+                colegioService.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "user1")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var dto = new ClaseDTO()
+            {
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1
+            };
+
+            var clase = new profefolio.Models.Entities.Clase()
+            {
+                Anho = 2023,
+                CicloId = 1,
+                ColegioId = 1,
+                Deleted = false,
+                Nombre = "Primer grado",
+                Turno = "Tarde",
+                Created = DateTime.Now,
+                CreatedBy = "juan.perez@gmail.com"
+            };
+
+            var ciclo = new profefolio.Models.Entities.Ciclo()
+            {
+                Id = 1,
+                Nombre = "Primero"
+            };
+
+            mapper.Setup(m => m.Map<profefolio.Models.Entities.Clase>(dto)).Returns(clase);
+
+            cicloService.Setup(c => c.FindById(dto.CicloId)).ReturnsAsync(ciclo);
+
+            colegioService.Setup(c => c.FindById(dto.ColegioId));
+
+            var result = await controller.Post(dto);
+
+            var response = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("El campo de Colegio es invalido", response.Value);
+        }
+
     }
 }
