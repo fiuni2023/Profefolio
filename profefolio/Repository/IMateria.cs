5ed2568d911e255cca331a38ff6ba1781b8f2b5a
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2023-03-25 23:09:50 -0300

    se hacen cambios de autorizacion.

--- profefolio/Repository/IMateria.cs
+++ profefolio/Repository/IMateria.cs
@@ -4,5 +4,6 @@ namespace profefolio.Repository;
 
 public interface IMateria : IRepository<Materia>
 {
+     Task<Materia> FindByNameMateriaId(string nombre, int id);
     Task<Materia> FindByNameMateria(string nombre);
 }
