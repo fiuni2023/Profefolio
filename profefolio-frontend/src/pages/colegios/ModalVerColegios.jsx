90124ae0d8b91841105ccd67424691cacd121190
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-03-28 22:22:14 -0400

    Se solucionaron los porblemas encontrados en Colegio

--- profefolio-frontend/src/pages/colegios/ModalVerColegios.jsx
+++ profefolio-frontend/src/pages/colegios/ModalVerColegios.jsx
@@ -3,7 +3,7 @@ import { Modal, Button } from 'react-bootstrap'
 import styles from "./ModalVerColegios.module.css"
 import axios from "axios";
 import { useGeneralContext } from "../../context/GeneralContext";
-import { toast } from 'react-hot-toast';
+
 import APILINK from '../../components/link';
 import { useNavigate } from "react-router-dom";
 
@@ -12,9 +12,9 @@ function ModalVerColegios({ idColegio, setShow, show }) {
   const [colegio, setColegio] = useState([]);
   const { getToken, verifyToken, cancan } = useGeneralContext();
   const nav = useNavigate();
-  const navigate = useNavigate();
+
   const [disabled, setDisabled] = useState(true);
-  const [nombreCompletoAdmin, setNombreCompletoAdmin]=useState("");
+
   useEffect(() => {
     console.log(idColegio);
     verifyToken()
@@ -38,9 +38,11 @@ function ModalVerColegios({ idColegio, setShow, show }) {
           console.log(error);
         });
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cancan, verifyToken, nav, getToken, idColegio])
 
-  const handleEdit=()=>{
+  const handleEdit = () => {
     setDisabled(false);
   }
   return (
@@ -56,11 +58,11 @@ function ModalVerColegios({ idColegio, setShow, show }) {
               <form>
                 <label htmlFor="colegio-nombre" className={styles.labelForm}>Nombre Colegio</label><br />
                 <input required type="text" id={styles.inputColegio} name="colegio-nombre" value={colegio.nombre || ''} disabled={disabled}></input><br />
-                
+
                 <label htmlFor="administrador"><strong> Administrador</strong></label><br />
-                <input required type="text" id={styles.inputColegio} name="colegio-admin" value={colegio.nombreAdministrador+" "+colegio.apellido || ''} disabled={disabled}></input><br />
-               
-               
+                <input required type="text" id={styles.inputColegio} name="colegio-admin" value={colegio.nombreAdministrador + " " + colegio.apellido || ''} disabled={disabled}></input><br />
+
+
 
 
               </form>
