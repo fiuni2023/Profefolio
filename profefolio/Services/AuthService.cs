3abc7facd30b39392579f5e423cb44ee250c3074
Carlos Torres <cctc2013@gmail.com>
2023-03-31 17:07:11 -0400

    Test a nivel de Service en el Login

--- profefolio/Services/AuthService.cs
+++ profefolio/Services/AuthService.cs
@@ -1,7 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using profefolio.Helpers;
 using profefolio.Models.DTOs.Auth;
 using profefolio.Models.Entities;
@@ -28,10 +27,7 @@ public class AuthService : IAuth
 
     public async Task<AuthPersonaDTO> Login(Login login)
     {
-        var user = await _userManager
-            .Users
-            .Where(p => !p.Deleted && p.Email.Equals(login.Email))
-            .FirstOrDefaultAsync();
+        var user = await _userManager.FindByEmailAsync(login.Email);
             
         
         if ((user == null || user.Deleted) || !await _userManager.CheckPasswordAsync(user, login.Password))
