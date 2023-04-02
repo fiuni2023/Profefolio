3080103f966b262d85170a3c2a657f2be656a2e5
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-02 00:55:13 -0400

    Se agrego el modelo de ColegioPersona y se agrego comprobaciones al DTO de creacion

---
+++ profefolio/Models/Entities/ColegioProfesor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.Entities
+{
+    public class ColegioProfesor : Data
+    {
+        [Required(ErrorMessage = "Es necesario indicar el Colegio")]
+        public int ColegioId { get; set; }
+
+        [Required(ErrorMessage = "Es necesario indicar el Profesor")]
+        public string? PersonaId { get; set; }
+    }
+}
