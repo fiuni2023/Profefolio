370ac657c0d9626e62417f918884bb3b8dad284a
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-02 01:11:18 -0400

    Se agrego la interface y el servicio que implementa la interface de ColegioProfesor

--- profefolio/Program.cs
+++ profefolio/Program.cs
@@ -65,6 +65,7 @@ builder.Services.AddAuthentication(options =>
 //Servicios
 builder.Services.AddScoped<IPersona, PersonasService>();
 builder.Services.AddScoped<IColegio, ColegiosService>();
+builder.Services.AddScoped<IColegioProfesor, ColegioProfesorService>();
 builder.Services.AddScoped<IMateria, MateriaService>();
 builder.Services.AddScoped<ICiclo, CicloService>();
 builder.Services.AddScoped<IClase, ClaseService>();
