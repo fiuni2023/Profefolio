f64cf2e9b3431a28a51309404995561e4c7b6402
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-01 20:27:16 -0400

    Se agrego un DTO para el resultado mandado al usuario cuando se guardeun nuevo ColegiosAlumnos

--- profefolio/Models/DTOs/ColegiosAlumnos/ColegiosAlumnosResultDTO.cs
+++ profefolio/Models/DTOs/ColegiosAlumnos/ColegiosAlumnosResultDTO.cs
@@ -5,9 +5,17 @@ using System.Threading.Tasks;
 
 namespace profefolio.Models.DTOs.ColegiosAlumnos
 {
-    public class ColegiosAlumnosDTO
+    public class ColegiosAlumnosResultDTO : DataDTO
     {
         public int ColegioId { get; set; }
         public string? AlumnoId { get; set; }
+
+        public string? Nombre {get; set;}
+
+        public string? Apellido { get; set; }
+
+        public string? Documento { get; set; }
+
+        public string? TipoDocumento { get; set; }
     }
 }
