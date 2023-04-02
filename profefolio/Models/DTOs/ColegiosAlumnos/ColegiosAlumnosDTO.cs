ffb222e70c20b8d8f5f0e388a14faac86894c473
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-01 20:15:58 -0400

    Se agrego un DTO para poder crear ColegiosAlumnos

---
+++ profefolio/Models/DTOs/ColegiosAlumnos/ColegiosAlumnosDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.DTOs.ColegiosAlumnos
+{
+    public class ColegiosAlumnosDTO
+    {
+        public int ColegioId { get; set; }
+        public int AlumnoId { get; set; }
+    }
+}
