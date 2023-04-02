b831c7daa0d4fd35ccc8a203a77d59d246e0c79a
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-01 20:34:14 -0400

    Se agrego el mapper del modelo al DTO result de ColegiosAlumnos

--- profefolio/Helpers/Mappers/ColegiosAlumnosMappper.cs
+++ profefolio/Helpers/Mappers/ColegiosAlumnosMappper.cs
@@ -12,12 +12,20 @@ namespace profefolio.Helpers.Mappers
     {
         public ColegiosAlumnosMappper()
         {
-        CreateMap<ColegiosAlumnos, ColegiosAlumnosDTO>()
-        .ForMember(dest => dest.AlumnoId, 
-            opt => opt.MapFrom(ca => ca.PersonaId))
-        .ForMember(dest => dest.ColegioId, 
-            opt => opt.MapFrom(ca => ca.ColegioId));
-            
+            CreateMap<ColegiosAlumnos, ColegiosAlumnosDTO>()
+            .ForMember(dest => dest.AlumnoId,
+                opt => opt.MapFrom(ca => ca.PersonaId))
+            .ForMember(dest => dest.ColegioId,
+                opt => opt.MapFrom(ca => ca.ColegioId)).ReverseMap();
+
+            CreateMap<ColegiosAlumnos, ColegiosAlumnosResultDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(ca => ca.Id))
+            .ForMember(dest => dest.AlumnoId, opt => opt.MapFrom(ca => ca.PersonaId))
+            .ForMember(dest => dest.ColegioId, opt => opt.MapFrom(ca => ca.ColegioId))
+            .ForMember(dest => dest.Nombre, opt => opt.MapFrom(ca => ca.Persona.Nombre))
+            .ForMember(dest => dest.Apellido, opt => opt.MapFrom(ca => ca.Persona.Apellido))
+            .ForMember(dest => dest.Documento, opt => opt.MapFrom(ca => ca.Persona.Documento))
+            .ForMember(dest => dest.TipoDocumento, opt => opt.MapFrom(ca => ca.Persona.DocumentoTipo));
         }
     }
 }
--- profefolio/Models/DTOs/ColegiosAlumnos/ColegiosAlumnosResultDTO.cs
+++ profefolio/Models/DTOs/ColegiosAlumnos/ColegiosAlumnosResultDTO.cs
@@ -9,13 +9,9 @@ namespace profefolio.Models.DTOs.ColegiosAlumnos
     {
         public int ColegioId { get; set; }
         public string? AlumnoId { get; set; }
-
         public string? Nombre {get; set;}
-
         public string? Apellido { get; set; }
-
         public string? Documento { get; set; }
-
         public string? TipoDocumento { get; set; }
     }
 }
