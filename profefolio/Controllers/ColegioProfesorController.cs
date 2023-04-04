6eb953c5cce2e67cf42ff17ea4978eb8fea38edc
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-04 00:36:43 -0400

    Se creo el DTO para editar la relacion Colegio-Profesor

--- profefolio/Controllers/ColegioProfesorController.cs
+++ profefolio/Controllers/ColegioProfesorController.cs
@@ -190,5 +190,82 @@ namespace profefolio.Controllers
                 return BadRequest("Error durante el guardado.");
             }
         }
+
+
+        
+        [HttpPost("{id:int}")]
+        [Authorize(Roles = "Administrador de Colegio")]
+        public async Task<ActionResult> Put(int id, [FromBody] ColegioProfesorEditDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Peticion invalido");
+            }
+
+            try
+            {
+
+                if (await _cProfService.Exist(dto.ProfesorId, dto.ColegioId))
+                {
+                    return BadRequest("Ya se registro el Profesor en este colegio");
+                }
+
+
+                var nameUser = User.FindFirstValue(ClaimTypes.Name);
+
+
+                //se verifica que exista un colegio en donde este asignado el administrador 
+                Colegio colegio = await _colegioService.FindById(dto.ColegioId);
+                if (colegio == null)
+                {
+                    return NotFound("El colegio no esta disponible");
+                }
+
+                //se verifica que el administrador tenga el mismo email que el del administrador que hizo la peticion
+                if (!colegio.personas.Email.Equals(nameUser))
+                {
+                    return Unauthorized("No puede agregar Profesores en otros colegios");
+                }
+
+                /*
+                    Se verifica que exista el profesor --- no se verifica que no sea nulo porque retorna una 
+                    excepcion si no encunetra
+                */
+                var persona = await _personaService.FindById(dto.ProfesorId);
+
+
+
+                var roles = await _personaService.GetRolesPersona(persona);
+
+                //se verifica que el id recibido sea de un profesor
+                if (!roles.Contains("Profesor"))
+                {
+                    return BadRequest("No se pueden asignar usuarios no asignados como profesor a los colegios");
+                }
+
+
+                var colProf = _mapper.Map<ColegioProfesor>(dto);
+
+                colProf.CreatedBy = nameUser;
+                colProf.Created = DateTime.Now;
+                colProf.Deleted = false;
+
+
+                await _cProfService.Add(colProf);
+                await _cProfService.Save();
+
+                return Ok(_mapper.Map<ColegioProfesorResultDTO>(colProf));
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+                return NotFound("El Profesor no esta disponible");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest("Error durante el guardado.");
+            }
+        }
     }
 }
