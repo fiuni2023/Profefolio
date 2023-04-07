e26f9b9243b38cbde7371f31a0626843c29115e4
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-06 20:56:19 -0400

    Se agrego un test para el caso en el que ocurra un error inesperado

--- TestProfefolio/Profesor/ProfesorTestinPuts.cs
+++ TestProfefolio/Profesor/ProfesorTestinPuts.cs
@@ -506,212 +506,70 @@ public class ProfesorTestinPuts
 
 
     // Generic exception
+    [Fact]
+    public async void Put_UnexpectedException_BadRequestk()
+    {
+        Mock<IMapper> mapper = new Mock<IMapper>();
+        Mock<IPersona> service = new Mock<IPersona>();
+        Mock<IRol> rol = new Mock<IRol>();
 
-    /*
-        [Fact]
-        public async void Put_NotFound_Id()
+        ProfesorController controller = new ProfesorController(mapper.Object, service.Object, rol.Object);
+
+        string id = "sd65sd6asd46asd4a6s5da6sd4a6s5da6";
+
+        profefolio.Models.Entities.Persona personaOld = new profefolio.Models.Entities.Persona()
         {
-            Mock<IMapper> mapper = new Mock<IMapper>();
-            Mock<IPersona> service = new Mock<IPersona>();
-            Mock<IRol> rol = new Mock<IRol>();
-
-            ProfesorController controller = new ProfesorController(mapper.Object, service.Object, rol.Object);
-
-            string id = "sd65sd6asd46asd4a6s5da6sd4a6s5da6";
-
-            profefolio.Models.Entities.Persona personaOld = new profefolio.Models.Entities.Persona()
-            {
-                Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da6",
-                UserName = "RamonRamirez",
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                EmailConfirmed = true,
-                Direccion = "Encarnacion",
-                EsM = true,
-                Nacimiento = nacimiento,
-                Created = nacimiento,
-                PhoneNumber = "0985123456"
-            };
-
-            profefolio.Models.Entities.Persona personaNew = new profefolio.Models.Entities.Persona()
-            {
-                Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da7",
-                UserName = "RamonRamirez",
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                EmailConfirmed = true,
-                Direccion = "Encarnacion",
-                EsM = true,
-                Nacimiento = nacimiento,
-                Created = nacimiento,
-                PhoneNumber = "0985123450"
-            };
+            Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da6",
+            UserName = "RamonRamirez",
+            Nombre = "Ramon",
+            Apellido = "Ramirez",
+            Documento = "7894689",
+            DocumentoTipo = "CI",
+            Email = "ramon.ramirez@gmail.com",
+            EmailConfirmed = true,
+            Direccion = "Encarnacion",
+            EsM = true,
+            Nacimiento = nacimiento,
+            Created = nacimiento,
+            PhoneNumber = "0985123456"
+        };
 
 
-            PersonaDTO personaDtoNew = new PersonaDTO()
-            {
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Direccion = "Encarnacion",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                Genero = "M",
-                Nacimiento = nacimiento,
-                Telefono = "0985123450",
-                Password = "12345678",
-                ConfirmPassword = "12345678"
-            };
-
-            PersonaResultDTO dtoResult = new PersonaResultDTO()
-            {
-                Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da7",
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Direccion = "Encarnacion",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Genero = "M",
-                Nacimiento = nacimiento,
-                Telefono = "0985123450"
-            };
-
-
-            service.Setup(p => p.FindById(id)).Throws(new FileNotFoundException());
-
-
-            var result = await controller.Put(id, personaDtoNew);
-
-            Assert.IsType<NotFoundObjectResult>(result);
-        }
-
-
-        [Fact]
-        public async void Put_BadRequest_EmailNewExist()
+        PersonaEditDTO personaDtoNew = new PersonaEditDTO()
         {
-            Mock<IMapper> mapper = new Mock<IMapper>();
-            Mock<IPersona> service = new Mock<IPersona>();
-            Mock<IRol> rol = new Mock<IRol>();
+            Nombre = "Ramon",
+            Apellido = "Ramirez",
+            Direccion = "Encarnacion",
+            Documento = "7894689",
+            DocumentoTipo = "CI",
+            Email = "ramon.ramirez@gmail.com",
+            Genero = "M",
+            Nacimiento = nacimiento,
+            Telefono = "0985123450"
+        };
 
-            ProfesorController controller = new ProfesorController(mapper.Object, service.Object, rol.Object);
 
-            string id = "sd65sd6asd46asd4a6s5da6sd4a6s5da6";
-
-            profefolio.Models.Entities.Persona personaOld = new profefolio.Models.Entities.Persona()
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
-                Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da6",
-                UserName = "RamonRamirez",
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                EmailConfirmed = true,
-                Direccion = "Encarnacion",
-                EsM = true,
-                Nacimiento = nacimiento,
-                Created = nacimiento,
-                PhoneNumber = "0985123456"
-            };
+                new Claim(ClaimTypes.Name, "user@gmail.com")
+            }, "role"));
 
-            profefolio.Models.Entities.Persona personaNew = new profefolio.Models.Entities.Persona()
-            {
-                Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da7",
-                UserName = "RamonRamirez",
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                EmailConfirmed = true,
-                Direccion = "Encarnacion",
-                EsM = true,
-                Nacimiento = nacimiento,
-                Created = nacimiento,
-                PhoneNumber = "0985123450"
-            };
-
-
-            PersonaDTO personaDtoNew = new PersonaDTO()
-            {
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Direccion = "Encarnacion",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                Genero = "M",
-                Nacimiento = nacimiento,
-                Telefono = "0985123450",
-                Password = "12345678",
-                ConfirmPassword = "12345678"
-            };
-
-            PersonaResultDTO dtoResult = new PersonaResultDTO()
-            {
-                Id = "sd65sd6asd46asd4a6s5da6sd4a6s5da7",
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Direccion = "Encarnacion",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Genero = "M",
-                Nacimiento = nacimiento,
-                Telefono = "0985123450"
-            };
-
-
-            service.Setup(p => p.FindById(id)).ReturnsAsync(personaOld);
-
-            mapper.Setup(m => m.Map<profefolio.Models.Entities.Persona>(personaDtoNew)).Returns(personaNew);
-
-            service.Setup(s => s.EditProfile(personaOld, personaNew, personaDtoNew.Password))
-                .Throws(new BadHttpRequestException("El email que desea actualizar ya existe"));
-
-            mapper.Setup(m => m.Map<PersonaResultDTO>(personaNew)).Returns(dtoResult);
-
-
-            var result = await controller.Put(id, personaDtoNew);
-            BadRequestObjectResult r = (BadRequestObjectResult)result.Result;
-
-            Assert.Equal("El email que desea actualizar ya existe", r.Value.ToString());
-        }
-
-        [Fact]
-        public async void Post_BadRequest_PasswordNull()
+        controller.ControllerContext = new ControllerContext()
         {
-            Mock<IMapper> mapper = new Mock<IMapper>();
-            Mock<IPersona> service = new Mock<IPersona>();
-            Mock<IRol> rol = new Mock<IRol>();
+            HttpContext = new DefaultHttpContext() { User = user }
+        };
 
-            ProfesorController controller = new ProfesorController(mapper.Object, service.Object, rol.Object);
+        service.Setup(p => p.FindById(It.IsAny<string>())).ReturnsAsync(personaOld);
+
+        service.Setup(p => p.ExistMail(It.IsAny<string>())).ReturnsAsync(true);
+
+        service.Setup(p => p.EditProfile(It.IsAny<Persona>())).ThrowsAsync(new Exception(""));
+
+        var result = await controller.Put(id, personaDtoNew);
 
 
-            PersonaDTO personaDto = new PersonaDTO()
-            {
-                Nombre = "Ramon",
-                Apellido = "Ramirez",
-                Direccion = "Encarnacion",
-                Documento = "7894689",
-                DocumentoTipo = "CI",
-                Email = "ramonramirez@gmail.com",
-                Genero = "M",
-                Nacimiento = nacimiento,
-                Telefono = "0985123456",
-                Password = null,
-                ConfirmPassword = "12345678"
-            };
+        var jsonResult = Assert.IsType<ConflictResult>(result.Result);
 
-            ActionResult<PersonaResultDTO> result = (await controller.Post(personaDto));
+    }
 
-            BadRequestObjectResult badRequestObjectResult = (BadRequestObjectResult)result.Result;
-            Assert.Equal("Falta el Password", badRequestObjectResult.Value.ToString());
-        }
-        */
 }
