2baa7e7564db62b3c8a4f395a4da79cc03f198ba
Carlos Torres <cctc2013@gmail.com>
2023-04-09 10:38:30 -0400

    Agregado clase para testear los nuevos cambios

---
+++ TestProfefolio/Account/AccountDeleteTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using profefolio.Controllers;
+using profefolio.Models.DTOs.Persona;
+using profefolio.Models.Entities;
+using profefolio.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProfefolio.Helpers;
+
+namespace TestProfefolio.Account
+{
+    public class AccountDeleteTest
+    {
+
+        private readonly Mock<IPersona> _personaServiceMock = new();
+        private readonly Mock<IRol> _rolServiceMock = new();
+        private readonly Mock<IColegio> _colegioServiceMock = new();
+        private readonly Mock<IMapper> _mapperMock = new();
+        private readonly AccountController _accountController;
+        private readonly string pathDtos = "../../../Account/Data/PersonasDTO.json";
+        private readonly string pathEntities = "../../../Account/Data/PersonasEnt.json";
+        private readonly JsonParser<Persona> _entityParser;
+        private readonly JsonParser<PersonaResultDTO> _dtoResultParser;
+        private readonly IEnumerable<PersonaResultDTO> _personasDto;
+
+        public AccountDeleteTest()
+        {
+            _accountController =
+                new AccountController
+                (
+                    _mapperMock.Object,
+                    _personaServiceMock.Object,
+                    _rolServiceMock.Object,
+                    _colegioServiceMock.Object
+                );
+
+            _entityParser = new JsonParser<Persona>(pathEntities);
+            _dtoResultParser = new JsonParser<PersonaResultDTO>(pathDtos);
+        }
+
+
+    }
+}
