8e3da4500723133f055e3e031ff2effcb2530153
Carlos Torres <cctc2013@gmail.com>
2023-04-10 16:21:03 -0400

    Agregado nuevo Repo para obtener los Administradores con o sin colegio asignado, creado el esqueleto del Servicio con el Patron Disposable

---
+++ profefolio/Repository/IAdmin.cs
@@ -0,0 +1,9 @@
+﻿using profefolio.Models.Entities;
+
+namespace profefolio.Repository
+{
+    public interface IAdmin : IDisposable
+    {
+        Task<IEnumerable<Persona>> GetUsersAssignedOrNotWithRoles(bool band, string role);
+    }
+}
---
+++ profefolio/Services/AdminReportService.cs
@@ -0,0 +1,44 @@
+﻿using profefolio.Models.Entities;
+using profefolio.Repository;
+
+namespace profefolio.Services
+{
+    public class AdminReportService : IAdmin
+    {
+        private bool disposedValue;
+
+        public Task<IEnumerable<Persona>> GetUsersAssignedOrNotWithRoles(bool band, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminar el estado administrado (objetos administrados)
+                }
+
+                // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+                // TODO: establecer los campos grandes como NULL
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+        // ~AdminReportService()
+        // {
+        //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
