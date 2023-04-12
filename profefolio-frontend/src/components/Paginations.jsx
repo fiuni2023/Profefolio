96508f0c9a53fd54190dc7acc94e8d7e3c5390a3
adaliz1 <ada.melgarejo@fiuni.edu.py>
2023-04-12 15:02:17 -0400

    correcciones

--- profefolio-frontend/src/components/Paginations.jsx
+++ profefolio-frontend/src/components/Paginations.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Pagination from 'react-bootstrap/Pagination';
-
-
-
+import styles from './Paginations.module.css'
+import IconButton from './IconButton'
+import { toast } from "react-hot-toast";
 const Paginations = ({ totalPage, currentPage, setCurrentPage, next }) => {
     const getPages = () => {
         return (
@@ -10,30 +10,46 @@ const Paginations = ({ totalPage, currentPage, setCurrentPage, next }) => {
                 <Pagination.Prev disabled={currentPage <= 0} onClick={() => {
                     setCurrentPage(currentPage - 1)
                 }} />
-                <Pagination.Item disabled >{currentPage + 1} DE {totalPage}</Pagination.Item>
+
                 <Pagination.Next disabled={!next} onClick={() => {
                     setCurrentPage(currentPage + 1)
-                }}/>
+                }} />
             </>
         )
     }
+    const getPage = (event) => {
+
+        if (event.target.value >= totalPage) {
+           toast.error("No existe la Pagina nro."+event.target.value)
+        }
+        else {
+            setCurrentPage(event.target.value);
+        }
+
+    }
     //Otra version de paginacion
     /*  <div className={styles.pagination}>
-            <button className={styles.buttonPag} disabled={currentPage <= 0} onClick={() => {
-                setCurrentPage(currentPage - 1)
-            }}><AiFillCaretLeft /></button>
+            
             <div className={styles.paginas}>{currentPage + 1} DE {totalPage}</div>
-            <button className={styles.buttonPag} disabled={!next} onClick={() => {
-                setCurrentPage(currentPage + 1)
-            }}><AiFillCaretRight /></button>
+            
         </div>
      */
     return (<>
 
-       
-        <Pagination size="sm mt-3" >
-            {getPages()}
-        </Pagination>
+        <div className={styles.pagination}>
+            <IconButton enabled={currentPage > 0} buttonType='arrowL' onClick={() => setCurrentPage(currentPage - 1)} />
+            <div className={styles.texto}>
+                <input type="number" value={currentPage + 1} onChange={event => getPage(event)} className={styles.inputCurrent} />
+
+                de {totalPage}
+            </div>
+
+            <IconButton enabled={next} buttonType='arrowR' onClick={() => setCurrentPage(currentPage + 1)} />
+
+
+
+        </div>
+
 
 
 
