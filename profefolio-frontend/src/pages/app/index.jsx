de767ca9e2a63f012de19f326781be7857118914
adaliz1 <ada.melgarejo@fiuni.edu.py>
2023-04-12 19:15:04 -0400

    correccion en listar colegios

--- profefolio-frontend/src/pages/app/index.jsx
+++ profefolio-frontend/src/pages/app/index.jsx
@@ -11,7 +11,7 @@ import Partidos from "../partidos";
 import CreatePartidos from "../partidos/components/create";
 import PartidosEdit from "../partidos/components/edit";
 import PartidosList from "../partidos/components/list";
-import Colegios from "../colegios/index.jsx"
+
 import ListarColegios from "../colegios/ListarColegios.jsx";
 import CreateProfesor from "../profesor/components/create/CreateModal.jsx";
 import ListarMaterias from "../materias/list/ListarMaTerias.jsx"
@@ -47,8 +47,8 @@ const App = () => {
                                     <Route path="list" element={<ListAdministrador />} />
                                 </Route>
 
-                                <Route path="/colegios" element={<Colegios />}>
-                                    <Route path="list" element={<ListarColegios/>}/>
+                                <Route path="/colegios/list" element={<ListarColegios />}>
+                                    
                                 </Route>
 
 
