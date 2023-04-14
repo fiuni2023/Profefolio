3a17318a677fee5aaf97848d229984d336106867
SoyReni <caballeroreni@gmail.com>
2023-04-13 21:11:04 -0400

    Dashboards con los nuevos cards implementados

---
+++ profefolio-frontend/src/pages/home/dashboards/adminHome/cards.jsx
@@ -0,0 +1,48 @@
+const Colegios = {
+    background: "orange",
+    hover: true,
+    goto: '/colegios/list',
+    header: {
+        title: "Colegios",
+    },
+    body: {
+        title: "Ultimos Colegios",
+        table: {
+            small: true, 
+            titulos: [ {titulo: "Nombre"}, {titulo: "Creado el"} ], 
+            filas: [
+                    {datos: [{dato: "Ceuce"}, {dato: "12/12/2017"}]},
+                    {datos: [{dato: "Cree"},  {dato: "02/02/2017"}]},
+                    {datos: [{dato: "Principito"}, {dato: "27/04/2017"}]},
+                    {datos: [{dato: "Inmaculada"}, {dato: "01/10/2017"}]},
+                    {datos: [{dato: "Morinigo"}, {dato: "31/01/2017"}]}
+            ]
+        }
+    }
+
+}
+
+const Administradores = {
+    background: "orange",
+    hover: true,
+    goto: '/administrador/list',
+    header: {
+        title: "Administradores",
+    },
+    body: {
+        title: "Ultimos Administradores",
+        table: {
+            small: true, 
+            titulos: [ {titulo: "Nombre"}, {titulo: "Apellido"}, {titulo: "Creado en"} ], 
+            filas: [
+                    {datos: [{dato: "John"}, {dato: "Doe"}, {dato: "12/12/2017"}]},
+                    {datos: [{dato: "Mery"}, {dato: "Jane"}, {dato: "02/02/2017"}]},
+                    {datos: [{dato: "Pepe"}, {dato: "Lepew"}, {dato: "27/04/2017"}]},
+                    {datos: [{dato: "Max"}, {dato: "Steele"}, {dato: "01/10/2017"}]},
+                    {datos: [{dato: "Laura"}, {dato: "Doe"}, {dato: "31/01/2017"}]}
+            ]
+        }
+    }
+}
+
+export {Colegios, Administradores};
