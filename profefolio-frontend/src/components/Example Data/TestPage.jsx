ca236d6b987401e03cadad07529772e4e4563c12
Camiroa <103272979+Camiroa@users.noreply.github.com>
2023-04-21 17:05:19 -0400

    creando y reutilizando componentes para lista

--- profefolio-frontend/src/components/Example Data/TestPage.jsx
+++ profefolio-frontend/src/components/Example Data/TestPage.jsx
@@ -1,26 +1,51 @@
 import React from 'react'
 import TextButton from '../TextButton'
 import IconButton from '../IconButton'
-
+import ScrolleableTable from '../ScrolleableTable'
 const TestPage = () => {
   const miFuncion = () => {
     console.log("Editando")
   }
+  //Datos de prueba
+  const Alumnos = {
+    xs: 12, sm: 12, md: 6, lg: 3,
+    background: "white",
+    hover: true,
+    goto: '',
+    header: {
+      title: "Alumnos",
+    },
+    body: {
+      table: {
+        small: true,
+        titulos: [{ titulo: "Nombre" }, { titulo: "Creado en" }],
+        filas: [
+          { datos: [{ dato: "John" }, { dato: "12/12/2017" }] },
+          { datos: [{ dato: "Mery" }, { dato: "02/02/2017" }] },
+          { datos: [{ dato: "Pepe" }, { dato: "01/10/2017" }] },
+          { datos: [{ dato: "Laura" }, { dato: "31/01/2017" }] }
+        ]
+      }
+    }
+
+  }
+
+
   return (
     <div>
       <div>
-
         <TextButton enabled={true} buttonType='accept' onClick={() => console.log('Aceptando')} />
         <IconButton enabled={true} buttonType='edit' onClick={miFuncion} />
         <IconButton enabled={true} buttonType='delete' onClick={miFuncion} />
-        <IconButton enabled={true} buttonType='edit2' onClick={miFuncion} />
-        <IconButton enabled={true} buttonType='delete2' onClick={miFuncion} />
         <IconButton enabled={true} buttonType='arrowL' onClick={() => console.log("Izquierda")} />
         <IconButton enabled={true} buttonType='arrowR' onClick={() => console.log("Derecha")} />
         <IconButton enabled={true} buttonType='close' onClick={() => console.log("Saliendo")} />
         <TextButton enabled={true} buttonType='cancel' onClick={() => console.log('Cancelando')} />
         <TextButton enabled={true} buttonType='save' onClick={() => console.log('Guardando')} />
         <TextButton enabled={true} buttonType='confirm' onClick={() => console.log('Confirmando')} />
+      </div>
+      <div>
+        <ScrolleableTable cardInfo={Alumnos}/>
       </div>
     </div>
   )
---
+++ profefolio-frontend/src/components/ScrolleableTable.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SCard, SBody, SHeader, SCol} from "./componentsStyles/StyledDashComponent";
+import Tabla from './Tabla';
+import { useNavigate } from 'react-router';
+
+function Card({cardInfo}){
+    const nav = useNavigate();
+    const handleClick = (goto) => {
+        nav(goto);
+    }
+    return (
+        <SCol xs={cardInfo?.xs ?? 12} sm={cardInfo?.sm ?? 12} md={cardInfo?.md ?? 6} lg={cardInfo?.lg ?? 4}>
+            <SCard onClick={cardInfo?.goto ? () => handleClick(cardInfo.goto) : null} hover={cardInfo?.hover ? "true" : "false" }>
+                {cardInfo?.header && 
+                    <SHeader background={cardInfo?.background ?? "gray"}>
+                        {cardInfo?.header?.title}</SHeader>}
+                {cardInfo?.body && 
+                    <SBody background={cardInfo?.background ?? "gray"}>
+                        {cardInfo?.body?.table && <Tabla datosTabla={cardInfo?.body?.table}></Tabla>}
+                    </SBody>}
+            </SCard>
+        </SCol>
+    )
+}
+
+export default Card; 
