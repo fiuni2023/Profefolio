e1ae355c4ca02149619cfb35afb9159efb826492
Camiroa <103272979+Camiroa@users.noreply.github.com>
2023-04-21 23:13:03 -0400

    agregando ci y apellido

--- profefolio-frontend/src/components/ScrolleableTable.jsx
+++ profefolio-frontend/src/components/ScrolleableTable.jsx
@@ -1,46 +1,41 @@
 import React from 'react';
-import { SCard, SBody, SHeader, SCol, SForm, Select, Item, List, ItemContainer, ListButton } from "./componentsStyles/StyledScrolleableList";
-import Tabla from './Tabla';
-import { useNavigate } from 'react-router';
+import { ScrollTable, SBody, SHeader, SForm, Container, Select, Item, List, ItemContainer, ListButton } from "./componentsStyles/StyledScrolleableList";
 import TextButton from './TextButton';
 import { RxReload } from 'react-icons/rx';
 
-function ListItem({ index, item, type, onClick }) {
-    console.log(item)
+function ListItem({ index, name, lastName, document, type, onClick }) {
     return (
         <ItemContainer type={type}>
-            <Item>{index}- {item}</Item>
-            <ListButton onClick={onClick}>{type!=='reload'? 'X' : <RxReload style={{fontSize: '24px'}} size={24} />}</ListButton>
+            <Item>{index}- {lastName} {name} - {document}</Item>
+            <ListButton onClick={onClick}>{type !== 'reload' ? 'X' : <RxReload style={{ fontSize: '24px' }} size={24} />}</ListButton>
         </ItemContainer>
     )
 }
 
 
 function Scrolleable({ cardInfo }) {
-    const nav = useNavigate();
-    const handleClick = (goto) => {
-        nav(goto);
-    }
-    console.log(cardInfo)
     return (
-        <SCol xs={cardInfo?.xs ?? 12} sm={cardInfo?.sm ?? 12} md={cardInfo?.md ?? 6} lg={cardInfo?.lg ?? 4}>
-            <SCard onClick={cardInfo?.goto ? () => handleClick(cardInfo.goto) : null} hover={"false"}>
+        <Container>
+            <ScrollTable>
                 {cardInfo?.header &&
-                    <SHeader background={cardInfo?.background ?? "gray"}>
-                        {cardInfo?.header?.title}</SHeader>}
+                    <SHeader>
+                        {cardInfo?.header?.title}
+                    </SHeader>}
                 {cardInfo?.list &&
                     <SBody background={cardInfo?.background ?? "gray"}>
                         <List>
                             {cardInfo?.list?.map((student, index) => (
-                                <ListItem
-                                    index={index+1}
-                                    item={student.name}
+                                <ListItem key={index}
+                                    index={index + 1}
+                                    name={student.name}
+                                    lastName={student.lastName}
+                                    document={student.document}
                                     type={student.status}
                                     onClick={() => console.log(`${student.name} 'seleccionado'`)} />
                             ))}
                         </List>
                     </SBody>}
-                <SForm onSubmit={cardInfo?.onSubmit ?? null}>
+                <SForm onSubmit={cardInfo?.onSubmit ?? null} >
                     <span>{cardInfo?.addTitle}</span>
                     <Select defaultValue={""}>
                         <option value="" disabled>{cardInfo?.selectTitle}</option>
@@ -50,13 +45,12 @@ function Scrolleable({ cardInfo }) {
                             </option>
                         ))}
                     </Select>
-
                     <div style={{ textAlign: 'right' }}>
                         <TextButton buttonType={'save-changes'} enabled={cardInfo?.enabled ?? false} />
                     </div>
                 </SForm>
-            </SCard>
-        </SCol>
+            </ScrollTable>
+        </Container>
     )
 }
 
