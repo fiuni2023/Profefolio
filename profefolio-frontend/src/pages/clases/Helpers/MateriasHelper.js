8292d04fc3b4db562d169de4a77419414868ede8
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-23 22:25:53 -0400

    se creo un helper de materia

---
+++ profefolio-frontend/src/pages/clases/Helpers/MateriasHelper.js
@@ -0,0 +1,56 @@
+import axios from "axios"
+import APILINK from "../../../components/link"
+
+const createMateria = async (body, token) => {
+    const result = await axios.post(`${APILINK}/api/materia`,
+        body,
+        {
+            headers: {
+                "Authorization": 'Bearer ' + token,
+                "Content-Type": "application/json"
+            },
+
+        })
+    return result.status === 200 ? result : null
+}
+
+const getMateriaNoAssigned = async (idClase, token) => {
+    const result = await axios.post(`${APILINK}/api/materia/NoAsignadas/${idClase}`,
+        {
+            headers: {
+                "Authorization": 'Bearer ' + token,
+                "Content-Type": "application/json"
+            },
+
+        })
+    return result.status === 200 ? result : null
+}
+
+
+const updateMateria= async (id, body, token) => {
+    const result = await axios.put(`${APILINK}/api/materia/${id}`,
+        body,
+        {
+            headers: {
+                "Authorization": 'Bearer ' + token,
+                "Content-Type": "application/json"
+            },
+
+        })
+    return result
+}
+
+const deleteMateria = async (id, token) => {
+    const result = await axios.delete(`${APILINK}/api/materia/${id}`,
+        {
+            headers: {
+                "Authorization": 'Bearer ' + token,
+                "Content-Type": "application/json"
+            },
+
+        })
+    return result.status === 200 ? result : null
+}
+
+const MateriasService = { createMateria, getMateriaNoAssigned, updateMateria, deleteMateria }
+export default MateriasService
