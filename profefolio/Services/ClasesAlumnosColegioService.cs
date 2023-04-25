18780414884a0a1b92227a753e239f09a81bb794
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-25 01:29:15 -0400

    Se agregaron los metodos necesarios en el service

--- profefolio/Services/ClasesAlumnosColegioService.cs
+++ profefolio/Services/ClasesAlumnosColegioService.cs
@@ -2,16 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using profefolio.Models;
 using profefolio.Models.Entities;
 using profefolio.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace profefolio.Services
 {
     public class ClasesAlumnosColegioService : IClasesAlumnosColegio
     {
-        public Task<ClasesAlumnosColegio> Add(ClasesAlumnosColegio t)
+        private ApplicationDbContext _context;
+        public ClasesAlumnosColegioService(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<ClasesAlumnosColegio> Add(ClasesAlumnosColegio t)
+        {
+            _context.Entry(t).State = EntityState.Added;
+            return await Task.FromResult(t);
         }
 
         public int Count()
@@ -19,14 +28,15 @@ namespace profefolio.Services
             throw new NotImplementedException();
         }
 
-        public void Dispose()
+        public async void Dispose()
         {
-            throw new NotImplementedException();
+            await _context.DisposeAsync();
         }
 
         public ClasesAlumnosColegio Edit(ClasesAlumnosColegio t)
         {
-            throw new NotImplementedException();
+            _context.Entry(t).State = EntityState.Modified;
+            return t;
         }
 
         public bool Exist()
@@ -34,9 +44,13 @@ namespace profefolio.Services
             throw new NotImplementedException();
         }
 
-        public Task<ClasesAlumnosColegio> FindById(int id)
+        public async Task<ClasesAlumnosColegio> FindById(int id)
         {
-            throw new NotImplementedException();
+            return await _context.ClasesAlumnosColegios
+                    .Where(a => !a.Deleted && a.Id == id)
+                    .Include(a => a.Clase)
+                    .Include(a => a.ColegiosAlumnos)
+                    .FirstOrDefaultAsync();
         }
 
         public IEnumerable<ClasesAlumnosColegio> GetAll(int page, int cantPorPag)
@@ -46,7 +60,7 @@ namespace profefolio.Services
 
         public Task Save()
         {
-            throw new NotImplementedException();
+            return _context.SaveChangesAsync();
         }
     }
 }
