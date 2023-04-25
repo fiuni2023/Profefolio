38b5d59e8822d732b75f0543ab3322e21d8a88fa
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-25 01:18:34 -0400

    Se agrego el service

---
+++ profefolio/Services/ClasesAlumnosColegioService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using profefolio.Models.Entities;
+using profefolio.Repository;
+
+namespace profefolio.Services
+{
+    public class ClasesAlumnosColegioService : IClasesAlumnosColegio
+    {
+        public Task<ClasesAlumnosColegio> Add(ClasesAlumnosColegio t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClasesAlumnosColegio Edit(ClasesAlumnosColegio t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClasesAlumnosColegio> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClasesAlumnosColegio> GetAll(int page, int cantPorPag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
