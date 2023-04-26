7ba622bc3daa6f0148135fa2fa6c25278a6bc64a
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-26 15:43:25 -0400

    Se agrego el dto a retornar

---
+++ profefolio/Models/DTOs/ClasesAlumnosColegio/ClaseAlumnosColegiosInfoAlumnoDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.DTOs.ClasesAlumnosColegio
+{
+    public class ClaseAlumnosColegiosInfoAlumnoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Documento { get; set; }
+        
+    }
+}
