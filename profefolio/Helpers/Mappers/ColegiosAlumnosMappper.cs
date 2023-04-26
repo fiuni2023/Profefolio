c3c395ef44d4c3b648b6590515ebac2879dceb0b
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-04-26 05:56:08 -0400

    Se arreglo el mapper

--- profefolio/Helpers/Mappers/ColegiosAlumnosMappper.cs
+++ profefolio/Helpers/Mappers/ColegiosAlumnosMappper.cs
@@ -30,19 +30,19 @@ namespace profefolio.Helpers.Mappers
 
             CreateMap<ColegiosAlumnos, ColegioAlumnoListPageDTO>()
                 .ForMember(dest => dest.Id, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Id))
+                    opt => opt.MapFrom(v => v.Persona.Id))
                 .ForMember(dest => dest.Documento, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Documento))
+                    opt => opt.MapFrom(v => v.Persona.Documento))
                 .ForMember(dest => dest.Nombre, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Nombre))
+                    opt => opt.MapFrom(v => v.Persona.Nombre))
                 .ForMember(dest => dest.Apellido, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Apellido))
+                    opt => opt.MapFrom(v => v.Persona.Apellido))
                 .ForMember(dest => dest.FechaNacimiento, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Nacimiento))
+                    opt => opt.MapFrom(v => v.Persona.Nacimiento))
                 .ForMember(dest => dest.Direccion, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Direccion))
+                    opt => opt.MapFrom(v => v.Persona.Direccion))
                 .ForMember(dest => dest.Email, 
-                    opt => opt.MapFrom(v => v.Colegio.personas.Email));
+                    opt => opt.MapFrom(v => v.Persona.Email));
         }
     }
 }
