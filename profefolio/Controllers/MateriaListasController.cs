e179d51a57e68f531937085abe323ae934c829ca
Carlos Torres <cctc2013@gmail.com>
2023-04-26 16:57:05 -0400

    Agregado controller put, logica movida al service

--- profefolio/Controllers/MateriaListasController.cs
+++ profefolio/Controllers/MateriaListasController.cs
@@ -89,62 +89,26 @@ namespace profefolio.Controllers
         [Route("{idClase:int}")]
         public async Task<ActionResult> Put(int idClase, [FromBody] ClaseMateriaEditDTO dto)
         {
-
-            if (idClase != dto.IdClase)
-            {
-                return BadRequest();
-            }
-
-            if (dto.IdProfesores.Count < 1) return BadRequest();
-
             var user = User.Identity.Name;
             try
             {
-                var clase = await _claseService.FindById(idClase);
-
-                var detalles = new Collection<MateriaLista>();
-
-                foreach (var item in dto.IdProfesores.Distinct())
-                {
-                    var p = await _profesorService.FindById(item);
-
-                    if (p == null) { continue; }
-
-                    var detalle = await _materiaListaService.Find(dto.IdClase, item, dto.IdMateria, user);
-
-                    if (detalle == null)
-                    {
-                        detalle = new MateriaLista
-                        {
-                            ClaseId = dto.IdClase,
-                            MateriaId = dto.IdMateria,
-                            ProfesorId = item
-                        };
-                    }
-                    else
-                    {
-                        detalle.ClaseId = dto.IdClase;
-                        detalle.MateriaId = dto.IdMateria;
-                        detalle.ProfesorId = item;
-                    }
-
-                    detalles.Add(detalle);
-
-                }
-
-
-                clase.MateriaListas = detalles;
-
-
-
-                _claseService.Edit(clase);
-                await _claseService.Save();
-                return Ok();
+                var query = await _materiaListaService.EditMateriaLista(dto, user);
+                if (query) return Ok();
             }
             catch (FileNotFoundException e)
             {
                 return NotFound();
             }
+            catch (BadHttpRequestException e)
+            {
+                return BadRequest(e.Message);
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                return Unauthorized();
+            }
+            return BadRequest("Error al ejecutar la secuencia de intrucciones");
+
         }
 
         [HttpDelete]
