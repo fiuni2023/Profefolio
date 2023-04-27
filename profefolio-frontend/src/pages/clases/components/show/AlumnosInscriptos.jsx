ff37337cfeac3ee1a5065f28970b735eafd55e0d
Camiroa <103272979+Camiroa@users.noreply.github.com>
2023-04-26 21:59:33 -0400

    setear valores de alumnos asignados y no asignados de una clase

--- profefolio-frontend/src/pages/clases/components/show/AlumnosInscriptos.jsx
+++ profefolio-frontend/src/pages/clases/components/show/AlumnosInscriptos.jsx
@@ -4,22 +4,40 @@ import StudentHelper from '../../../alumnos/helpers/StudentHelper'
 import { useFetchEffect } from '../../../../components/utils/useFetchEffect'
 import { useGeneralContext } from '../../../../context/GeneralContext';
 import { toast } from 'react-hot-toast';
-const AlumnosInscriptos = ({colegio}) => {
+import { useClaseContext } from '../../context/ClaseContext';
+const AlumnosInscriptos = () => {
     const [listaAlumnos, setListaAlumnos] = useState([])
+    const [alumnosSelect, setAlumnosSelect] = useState([])
+    const { getClaseSelectedId} = useClaseContext();
     const { getToken} = useGeneralContext();
     const { loading } = useFetchEffect(
         () => {
-            return StudentHelper.getStudentsPage(0, getToken())
+            return StudentHelper.getAllClassStudents( getClaseSelectedId(), getToken())
         },
         [ getToken],
         {
             condition: true,
             handleSuccess: (r) => {
                 setListaAlumnos(r.data.dataList)
-                console.log(r.data.dataList)
             },
             handleError: () => {
-                toast.error("No se pudieron obtener los alumnos. Intente recargar la página")
+                toast.error("No se pudieron obtener los alumnos de la clase. Intente recargar la página")
+            }
+        }
+    )
+    const { loadingSelect } = useFetchEffect(
+        () => {
+            return StudentHelper.getAllNotClassStudents( getClaseSelectedId(), getToken())
+        },
+        [ getToken],
+        {
+            condition: true,
+            handleSuccess: (r) => {
+                setAlumnosSelect(r.data)
+                console.log(r.data)
+            },
+            handleError: () => {
+                toast.error("No se pudieron obtener los alumnos para seleccionar. Intente recargar la página")
             }
         }
     )
@@ -35,10 +53,11 @@ const AlumnosInscriptos = ({colegio}) => {
             { label: "Carlos", value: 1 },
             { label: "Gabriela", value: 1 }
         ],
-        list: listaAlumnos
+        list: listaAlumnos,
+        options: alumnosSelect
     }
     return <>
-        <ScrolleableTable isLoading={loading} studentsList={Alumnos} />
+        <ScrolleableTable isLoading={loading} loadingSelect={loadingSelect} studentsList={Alumnos} />
     </>
 
 }
