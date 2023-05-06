99b194d8cf1ea820127b4163e3ea090f5afae6fd
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-06 02:26:38 -0400

    Se creo el service de HoraCatedra

---
+++ profefolio/Services/HoraCatedraService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using profefolio.Models.Entities;
+using profefolio.Repository;
+
+namespace profefolio.Services
+{
+    public class HoraCatedraService : IHoraCatedra
+    {
+        public Task<HoraCatedra> Add(HoraCatedra t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HoraCatedra Edit(HoraCatedra t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HoraCatedra> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<HoraCatedra> GetAll(int page, int cantPorPag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
