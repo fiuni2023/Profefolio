55dc04f506bb34f3b1a2b0a9b97e7cf00274305c
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-06 02:25:21 -0400

    Se agrego el repositorio de HoraCatedra

---
+++ profefolio/Repository/IHoraCatedra.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using profefolio.Models.Entities;
+
+namespace profefolio.Repository
+{
+    public interface IHoraCatedra : IRepository<HoraCatedra>
+    {
+        
+    }
+}
