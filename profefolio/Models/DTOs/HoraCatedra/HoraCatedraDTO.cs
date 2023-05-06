0164cdc6e212ebb06ca97c423de4801420137626
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-06 03:38:18 -0400

    Se agrego el DTO para el metodo POST

--- profefolio/Models/DTOs/HoraCatedra/HoraCatedraDTO.cs
+++ profefolio/Models/DTOs/HoraCatedra/HoraCatedraDTO.cs
@@ -4,9 +4,9 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace profefolio.Models.Entities
+namespace profefolio.Models.DTOs.HoraCatedra
 {
-    public class HoraCatedra : Data
+    public class HoraCatedraDTO
     {
         [Required(ErrorMessage = "La hora de inicio es requerida")]
         [RegularExpression(@"\b((0|[01][0-9]|2[0-3]):[0-5][0-9]|00:00)\b", ErrorMessage = "El formato debe ser HH:mm con dos digitos en cada cual y estar en el rango e 00:00 y 23:59")]
