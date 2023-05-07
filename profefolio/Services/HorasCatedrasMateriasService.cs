dae0e224dec1e1aabdc249289f8417c965d3af6b
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-06 22:35:14 -0400

    Se agrego el service

---
+++ profefolio/Services/HorasCatedrasMateriasService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using profefolio.Models.Entities;
+using profefolio.Repository;
+
+namespace profefolio.Services
+{
+    public class HorasCatedrasMateriasService : IHorasCatedrasMaterias
+    {
+        public Task<HorasCatedrasMaterias> Add(HorasCatedrasMaterias t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HorasCatedrasMaterias Edit(HorasCatedrasMaterias t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HorasCatedrasMaterias> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<HorasCatedrasMaterias> GetAll(int page, int cantPorPag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
