3544348ab185bf06f4ebf913c6437e459cc72e67
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-08 02:22:29 -0400

    se agrego un archivo de objetos estaricos

---
+++ profefolio-frontend/src/pages/profesor-pages/api/calendar.js
@@ -0,0 +1,72 @@
+export const Calendar = [
+        {
+            "nombre": "Lengua",
+            "nombreClase": "Septimo Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Lunes",
+            "inicio": "07:30",
+            "fin": "08:10",
+            "id": 1
+        },
+        {
+            "nombre": "Matematicas",
+            "nombreClase": "Sexto Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Martes",
+            "inicio": "08:10",
+            "fin": "08:50",
+            "id": 3
+        },
+        {
+            "nombre": "Lengua",
+            "nombreClase": "Septimo Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Miercoles",
+            "inicio": "13:40",
+            "fin": "14:20",
+            "id": 4
+        },
+        {
+            "nombre": "Matematicas",
+            "nombreClase": "Sexto Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Miercoles",
+            "inicio": "08:10",
+            "fin": "08:50",
+            "id": 5
+        },
+        {
+            "nombre": "Lengua",
+            "nombreClase": "Septimo Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Martes",
+            "inicio": "13:40",
+            "fin": "14:20",
+            "id": 6
+        },
+        {
+            "nombre": "Matematicas",
+            "nombreClase": "Sexto Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Jueves",
+            "inicio": "08:10",
+            "fin": "08:50",
+            "id": 7
+        },
+        {
+            "nombre": "Lengua",
+            "nombreClase": "Septimo Grado",
+            "colegioId": 2,
+            "nombreColegio": "San Jose 2",
+            "dia": "Jueves",
+            "inicio": "13:40",
+            "fin": "14:20",
+            "id": 8
+        }
+    ]
