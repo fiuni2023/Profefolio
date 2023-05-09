2c60a313c18fa19e9d0edf2ff5c0c92e85ba8f28
ErickKikuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-05-09 00:06:11 -0400

    Create clase.js

---
+++ profefolio-frontend/src/sevices/clase.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import APILINK from "../components/link";
+
+const getFirstPage = async (token)=>{
+    const result = await axios.get(`${APILINK}/api/Alumnos/page/0`,
+    {
+        headers:{
+            Authorization: 'Bearer ' + token
+        }
+    })
+    return result.status === 200? result : null
+}
+
+const ClaseService = {getFirstPage}
+export default ClaseService
