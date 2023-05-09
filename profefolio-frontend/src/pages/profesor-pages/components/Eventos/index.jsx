54a842a4f133d916537831d08d5c2d2afdafc1b9
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-09 05:31:22 -0400

    Se agrego la tabla inicial

--- profefolio-frontend/src/pages/profesor-pages/components/Eventos/index.jsx
+++ profefolio-frontend/src/pages/profesor-pages/components/Eventos/index.jsx
@@ -1,23 +1,48 @@
 import React from 'react'
 
+import { SBody, SCard, SHeader } from '../../../../components/componentsStyles/StyledDashComponent'
+import { TD, TH, TR, Table, Tbody, Thead } from '../../../../components/componentsStyles/StyledTable'
+
+const TableEvents = () => {
+    return <>
+        <Table style={{ width: "100%" }}>
+            <Thead>
+                <TR>
+                    <TH>Tipo</TH>
+                    <TH>Fecha</TH>
+                    <TH>Materia</TH>
+                    <TH>Clase</TH>
+                    <TH>Colegio</TH>
+                </TR>
+            </Thead>
+            <Tbody>
+                <TR>
+                    <TD>Ex</TD>
+                    <TD>12/05/2023</TD>
+                    <TD>Matematicas</TD>
+                    <TD>Quinto Grado</TD>
+                    <TD>San Juan</TD>
+                </TR>
+                <TR>
+                    <TD>Ex</TD>
+                    <TD>12/05/2023</TD>
+                    <TD>Matematicas</TD>
+                    <TD>Quinto Grado</TD>
+                    <TD>San Juan</TD>
+                </TR>
+            </Tbody>
+        </Table>
+    </>
+}
+
 const Eventos = () => {
     return <>
-        <div className="container-visualizacion">
-            Eventos
-        </div>
-
-        <style jsx="true">
-            {
-                `
-                    .container-visualizacion{
-                        border: 1px solid black;
-                        border-radius: 20px;
-                        background-color: #f4A89E;
-                        min-height: 300px;
-                    }
-                `
-            }
-        </style>
+        <SCard>
+            <SHeader>Horarios de Clases</SHeader>
+            <SBody>
+                <TableEvents />
+            </SBody>
+        </SCard>
     </>
 }
 
