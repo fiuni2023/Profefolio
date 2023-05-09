2f47c0bd661d5ece6fccde4ea8c11fb21304a6c4
ErickKikuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-05-08 20:25:20 -0400

    algunos cambios y definicion del componente

--- profefolio-frontend/src/components/UserDD.jsx
+++ profefolio-frontend/src/components/UserDD.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useGeneralContext } from "../context/GeneralContext";
 import styles from './UserDD.module.css'
+import { BsChevronCompactUp } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
 
 const UserDD = ({ showSB = false, setShowSB = ()=>{}}) => {
     
     const navigate = useNavigate()
-    const { isLogged, setIsLogged} = useGeneralContext()
+    const { isLogged, setIsLogged, getUserName } = useGeneralContext()
 
     const handleLogOut = () => {
+        setShowSB(false)
         localStorage.removeItem('loginData')
         sessionStorage.removeItem('loginData')
         navigate("/")
@@ -17,7 +20,7 @@ const UserDD = ({ showSB = false, setShowSB = ()=>{}}) => {
 
     const style = {
         height: showSB? "100%" : "0%",
-        paddingTop: showSB? "20px": "0px"
+        paddingTop: showSB? "2px": "0px"
     }
 
     return <>
@@ -28,7 +31,25 @@ const UserDD = ({ showSB = false, setShowSB = ()=>{}}) => {
             setShowSB(!showSB)
         }}>
             <div className={styles.background} style={style} onClick={(e)=>{e.stopPropagation()}}>
-                
+                {showSB &&
+                <>
+                    <div className={styles.ExitContainer} onClick={(e)=>{
+                        e.stopPropagation()
+                        setShowSB(!showSB)
+                    }}>
+                        <BsChevronCompactUp size={20}/>
+                    </div>
+                    <div className={styles.sbt}>
+                        {getUserName()}
+                    </div>
+                    <div className={styles.sbt}>
+                        <FaUserCircle size={100}/>
+                    </div>
+                    <div className={styles.sbt2} onClick={handleLogOut}>
+                        CERRAR SESIÓN
+                    </div>
+                </>
+                }
             </div>
         </div>
     </>
