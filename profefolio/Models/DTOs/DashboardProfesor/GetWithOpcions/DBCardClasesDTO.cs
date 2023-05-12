e5d2228eb00b34fde517363a79b31eafae2134a1
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-12 01:58:17 -0400

    Dto para el get de los datos de los cards de clase

---
+++ profefolio/Models/DTOs/DashboardProfesor/GetWithOpcions/DBCardClasesDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.DTOs.DashboardProfesor.GetWithOpcions
+{
+    public class DBCardClasesDTO : DataDTO
+    {
+        public string Ciclo { get; set; } = "";
+        public int Anho { get; set; } = 0;
+        public int Alumnos { get; set; } = 0;
+        public List<string> Materias { get; set; } = new List<string>();
+        public DBCardClasesHorariosDTO Horario { get; set; } = new();
+
+    }
+
+    
+}
