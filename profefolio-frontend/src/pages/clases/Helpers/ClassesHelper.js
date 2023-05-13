44cc5b20e74e19afdc26bb2a1c633d3566b3194f
dahybarboza <126834904+dahybarboza@users.noreply.github.com>
2023-05-13 17:45:59 -0400

    Cambiar la api de get list materia/profesores

--- profefolio-frontend/src/pages/clases/Helpers/ClassesHelper.js
+++ profefolio-frontend/src/pages/clases/Helpers/ClassesHelper.js
@@ -44,7 +44,7 @@ const getClassesByIdNombre = async (id, token) => {
 
 //
 const getMateriasProfesores= async (id, token) => {
-      const result = await axios.get(`${APILINK}/api/lista/materias/ConProfesores/${id}`, {
+      const result = await axios.get(`${APILINK}/api/administrador/materia/profesores/${id}`, {
         headers: {
             "Authorization": 'Bearer ' + token,
             "Content-Type": "application/json"
