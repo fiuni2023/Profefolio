c5ad96e241cda697d5d41e6db360b1969d0abfce
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-15 20:01:15 -0400

    se solucionaron problemas al mapear la respuesta de la api

--- profefolio-frontend/src/pages/profesor-pages/components/Eventos/TableEvents.jsx
+++ profefolio-frontend/src/pages/profesor-pages/components/Eventos/TableEvents.jsx
@@ -8,89 +8,6 @@ import { useGeneralContext } from '../../../../context/GeneralContext';
 
 const TableEvents = memo(({ style = {} }) => {
 
-    /*     const eventos = [
-            {
-                id: 1,
-                tipo: "Examen",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 2,
-                tipo: "Evento",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 3,
-                tipo: "Parcial",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 4,
-                tipo: "Prueba Sumativa",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 5,
-                tipo: "Parcial",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 6,
-                tipo: "Prueba Sumativa",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 7,
-                tipo: "Prueba Sumativa",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 8,
-                tipo: "Examen",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 7,
-                tipo: "Prueba Sumativa",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            },
-            {
-                id: 8,
-                tipo: "Examen",
-                fecha: new Date(),
-                materia: "Matematicas",
-                clase: "Primer Grado",
-                colegio: "San Juan"
-            }
-        ] */
-
     const { getToken } = useGeneralContext();
 
     const [eventos, setEventos] = useState([]);
@@ -118,13 +35,20 @@ const TableEvents = memo(({ style = {} }) => {
                     </TR>
                 </Thead>
                 <Tbody>
-                    {map(eventos, (e, i) => <TR key={i} style={{ backgroundColor: Tools.GetColorEvento(e.tipo) }}>
-                        <TD className='col-tipo'>{e.tipo}</TD>
-                        <TD>{e.fecha.toLocaleDateString("en-US", { day: "2-digit", month: "2-digit", year: "numeric" })}</TD>
-                        <TD className="col-materia">{e.nombreMateria}</TD>
-                        <TD>{e.nombreClase.split(" ").length === 2 ? `${e.clase.split(" ")[0].at(0)}.${e.clase.split(" ")[1].at(0)}.` : e.clase.split("")}</TD>
-                        <TD className="col-colegio">{e.nombreColegio}</TD>
-                    </TR>)}
+                    {
+                        !!eventos && eventos.length > 0
+                            ?
+                            map(eventos, (e, i) => <TR key={i} style={{ backgroundColor: Tools.GetColorEvento(e.tipo) }}>
+                                <TD className='col-tipo'>{e.tipo}</TD>
+                                <TD>{new Date(e.fecha).toLocaleDateString("en-US", { day: "2-digit", month: "2-digit", year: "numeric" })}</TD>
+                                <TD className="col-materia">{e.nombreMateria}</TD>
+                                <TD>{e.nombreClase.split(" ").length === 2 ? `${e.nombreClase.split(" ")[0].at(0)}.${e.nombreClase.split(" ")[1].at(0)}.` : e.nombreClase.split("")}</TD>
+                                <TD className="col-colegio">{e.nombreColegio}</TD>
+                            </TR>)
+                            :
+                            <TR>
+                                <TD style={{ textAlign: "center", fontWeight: "bold" }} colSpan={5}>No hay eventos futuros.</TD>
+                            </TR>}
 
                 </Tbody>
             </Table>
