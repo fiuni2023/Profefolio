ef03d6e9962f6cd5cc5e752f336dc23c46888f6d
ErickKikuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-05-16 22:36:15 -0400

    Create index.jsx

---
+++ profefolio-frontend/src/pages/profesor-pages/components/PromedioAsistencia/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SBody, SCard, SHeader } from '../../../../components/componentsStyles/StyledDashComponent'
+
+const PromedioAsistencia = () => {
+    return <>
+        <SCard>
+            <SHeader>Promedio de Asistencia</SHeader>
+            <SBody>
+            </SBody>
+        </SCard>
+    </>
+}
+
+export default PromedioAsistencia
