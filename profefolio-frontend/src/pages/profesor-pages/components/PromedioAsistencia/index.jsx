873abbae586b75d0d7714395caca7d228d7f9340
ErickKikuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-05-17 10:00:14 -0400

    added Bar chart asistencia

--- profefolio-frontend/src/pages/profesor-pages/components/PromedioAsistencia/index.jsx
+++ profefolio-frontend/src/pages/profesor-pages/components/PromedioAsistencia/index.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import { SBody, SCard, SHeader } from '../../../../components/componentsStyles/StyledDashComponent'
+import { Bar } from 'react-chartjs-2';
 
 const PromedioAsistencia = () => {
+    const labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Deciembre'];
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Presente',
+                data: labels.map(() => parseInt(Math.random()*100)),
+                backgroundColor: '#24B787',
+            },
+            {
+                label: 'Ausente',
+                data: labels.map(() => parseInt(Math.random()*100)),
+                backgroundColor: '#D93D79',
+            },
+            {
+                label: 'Justificado',
+                data: labels.map(() => parseInt(Math.random()*100)),
+                backgroundColor: '#5181D1',
+            },
+        ],
+    };
+
     return <>
         <SCard>
             <SHeader>Promedio de Asistencia</SHeader>
             <SBody>
+                <Bar options={options} data={data} />
             </SBody>
         </SCard>
     </>
