1aca6671392e567ad1142e3ee72dd559daf8a5f9
ErickKikuchi <101877459+KaitoK1kuchi@users.noreply.github.com>
2023-05-16 22:36:11 -0400

    Create index.jsx

---
+++ profefolio-frontend/src/pages/profesor-pages/components/PromedioPuntaje/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SBody, SCard, SHeader } from '../../../../components/componentsStyles/StyledDashComponent'
+
+const PromedioPuntaje = () => {
+    return <>
+        <SCard>
+            <SHeader>Promedio de Puntaje</SHeader>
+            <SBody>
+            </SBody>
+        </SCard>
+    </>
+}
+
+export default PromedioPuntaje
