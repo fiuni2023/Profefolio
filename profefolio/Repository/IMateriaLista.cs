8c7c3b413326dbec5444d31ba5b75a323481b5d9
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-05-17 23:28:22 -0400

    modify interface

--- profefolio/Repository/IMateriaLista.cs
+++ profefolio/Repository/IMateriaLista.cs
@@ -6,7 +6,6 @@ namespace profefolio.Repository
     public interface IMateriaLista : IDisposable
     {
         Task<bool> IsUsedMateria(int idMateria);
-        Task<MateriaLista> Find(int idClase, string idProfesor, int idMateria, string userLogged);
         Task<ClaseDetallesDTO> FindByIdClase(int idClase, string user);
         Task<List<MateriaLista>> FindByIdClaseAndUser(int idClase, string userEmail, string role);
 
