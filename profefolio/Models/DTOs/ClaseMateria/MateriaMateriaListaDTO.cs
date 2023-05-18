7845754b2ad9ebc78c1f315a1d786a12d1e6f5e0
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-05-17 23:55:25 -0400

    dto formado

--- profefolio/Models/DTOs/ClaseMateria/MateriaMateriaListaDTO.cs
+++ profefolio/Models/DTOs/ClaseMateria/MateriaMateriaListaDTO.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace profefolio
 {
     public class MateriaMateriaListaDTO
     {
+        [Required(ErrorMessage = "Requerido")]
         public int IdMateria {get; set;}
+        [Required(ErrorMessage = "Requerido")]
+        public List<ProfesoresEstadosDTO> Profesores {get; set;} 
     }
     
 }
