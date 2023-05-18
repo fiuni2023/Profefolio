d8e5c585acec8aa77270d58902eb00923b45e8a1
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-05-17 23:37:59 -0400

    revertido

---
+++ profefolio/Models/DTOs/ClaseMateria/ClaseMateriaProfesorDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.DTOs.ClaseMateria
+{
+    public class ClaseMateriaProfesorDTO
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+    }
+}
