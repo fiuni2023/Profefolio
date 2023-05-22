6d97e2e90f778128f224aeed6248a62ba0f4b8f2
Camiroa <103272979+Camiroa@users.noreply.github.com>
2023-05-22 02:02:02 -0400

    agregar estilos de componentes de página asistencia

---
+++ profefolio-frontend/src/pages/profesor-pages/pages/asistencia/componentes/StyledResumenAsistencia.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Resumen =  styled.div`
+    flex: right;
+    padding: 10px;
+    padding-inline: 20px;
+    border: 1px solid gray;
+    border-radius: 15px;
+    margin: 20px;
+    p{
+        font-weight: 500;
+        font-size: 14px;
+        margin-bottom: 10px;
+    }
+`;
+export const SideSection = styled.div`
+    display: inline-flex;
+`;
+export const Container = styled.div`
+    display: inline-flex;
+    width: 100%;
+    justify-content: space-between;
+`;
