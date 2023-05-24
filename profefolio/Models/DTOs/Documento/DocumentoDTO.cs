c6fa2f827ae3c755438c2b57133ac99fc183a2e8
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2023-05-24 19:48:08 -0400

    se crea DocumentoResultDTO

--- profefolio/Models/DTOs/Documento/DocumentoDTO.cs
+++ profefolio/Models/DTOs/Documento/DocumentoDTO.cs
@@ -1,14 +1,12 @@
-namespace profefolio.Models.DTOs.Evento;
+namespace profefolio.Models.DTOs.Documento;
 using Newtonsoft.Json;
 
 public class DocumentoDTO
 {
-     [Required(ErrorMessage = "Se tiene que agregar un nombre")]
-        [MaxLength(32)]
+    
         public string? Nombre { get; set; }
 
-        [Required(ErrorMessage = "Se tiene que agregar un enlace")]
-        [MaxLength(100)]
+       
         public string? Enlace { get; set; }
 
         //profesor que creo el documento
---
+++ profefolio/Models/DTOs/Documento/DocumentoResultDTO.cs
@@ -0,0 +1,8 @@
+namespace profefolio.Models.DTOs.Documento;
+public class DocumentoResultDTO: DataDTO
+{
+   
+        public string? Nombre { get; set; }
+        public string? Enlace { get; set; }
+    
+}
