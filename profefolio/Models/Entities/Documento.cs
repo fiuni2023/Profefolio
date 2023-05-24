c1e5690f1ce8c6fae4293e7d042ac431e0e1a7c0
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2023-05-24 19:42:02 -0400

    se crea la entidad documento

---
+++ profefolio/Models/Entities/Documento.cs
@@ -0,0 +1,20 @@
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace profefolio.Models.Entities
+{
+    public class Documento : Data
+    {
+        [Required(ErrorMessage = "Se tiene que agregar un nombre")]
+        [MaxLength(32)]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Se tiene que agregar un enlace")]
+        [MaxLength(100)]
+        public string? Enlace { get; set; }
+    }
+}
