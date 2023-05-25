a27ebf3a508ad05353f1b717468e7315912a1e12
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-24 23:00:27 -0400

    se agrego el dto para el body del get

---
+++ profefolio/Models/DTOs/Asistencia/AsistenciaGetDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.DTOs.Asistencia
+{
+    public class AsistenciaGetDTO
+    {
+        public int IdMateriaLista{get; set;}
+        public int Mes { get; set; }
+    }
+}
