8a1698be1bdd7e3076ea7f44c33a3d708d95fb1e
Camiroa <103272979+Camiroa@users.noreply.github.com>
2023-05-26 02:23:46 -0400

    removiendo handler

--- profefolio-frontend/src/pages/profesor-pages/pages/asistencia/componentes/DayMonthPicker.jsx
+++ profefolio-frontend/src/pages/profesor-pages/pages/asistencia/componentes/DayMonthPicker.jsx
@@ -1,21 +1,15 @@
-const DayMonthPicker = ({ selectedDate = () => new Date()}) => {
+const DayMonthPicker = () => {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
   
-  const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  };
-
   return (
     <input
+      id= "fechaSeleccionada"
       type="date"
       style={{ width: '90px' , borderRadius: '10px', border: 'solid 1px'}} 
       min={`${currentYear}-01-01`}
       max={`${currentYear}-12-31`}
-      defaultValue={formatDate(selectedDate)}
+      defaultValue={currentDate.toISOString().split('T')[0]}
     />
   );
 };
