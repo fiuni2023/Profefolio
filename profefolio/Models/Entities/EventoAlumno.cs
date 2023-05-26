170e945a3c94adcbbaae9e6c1bbfb7cf10bab3d2
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-05-26 12:03:30 -0400

    relacion EnventoAlumno Calificacion

--- profefolio/Models/Entities/EventoAlumno.cs
+++ profefolio/Models/Entities/EventoAlumno.cs
@@ -1,9 +1,11 @@
 namespace profefolio.Models.Entities;
 
 public class EventoAlumno : Data
-{  
+{
     public Evento Evento { get; set; }
     public Persona Alumno { get; set; }
     public double PuntajeLogrado { get; set; }
+    public double PorcentajeLogrado { get; set; }
+    public Calificacion Calificacion { get; set; }
     public int IdCalificacion { get; set; }
 }
