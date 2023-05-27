29c5e1eb9974814bdee29200f34bc8a96a43873d
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-27 00:12:36 -0400

    Back se recupero DBClaseAlumnoColegioDTO

---
+++ profefolio/Models/DTOs/DashboardProfesor/GetWithOpcions/DBClaseAlumnoColegioDTO.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace profefolio.Models.DTOs.DashboardProfesor.GetWithOpcions
+{
+    public class DBClaseAlumnoColegioDTO : DataDTO
+    {
+        public string Nombres { get; set; } = "";
+        public string Apellidos { get; set; } = "";
+
+    }
+
+    
+}
