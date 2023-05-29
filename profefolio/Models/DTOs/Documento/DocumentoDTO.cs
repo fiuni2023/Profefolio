cd6c2ed07d6e9ed6de5d4ee07f079bded9b8a2e4
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2023-05-28 22:25:39 -0400

    se modifica el DTO documento

--- profefolio/Models/DTOs/Documento/DocumentoDTO.cs
+++ profefolio/Models/DTOs/Documento/DocumentoDTO.cs
@@ -5,13 +5,7 @@ public class DocumentoDTO
 {
     
         public string? Nombre { get; set; }
-
-       
         public string? Enlace { get; set; }
-
-        //profesor que creo el documento
-        [JsonIgnore] // Agrega el atributo JsonIgnore al campo ProfesorId
-        public String? ProfesorId { get; set; }
         public int MateriaListaId{ get; set; }
     
 }
