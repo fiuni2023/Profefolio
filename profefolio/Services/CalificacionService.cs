611c21cbcbbc4da9b9a7364bb9243e877bf80809
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-03 18:25:50 -0400

    Creado CalificacionService.cs

---
+++ profefolio/Services/CalificacionService.cs
@@ -0,0 +1,17 @@
+using profefolio.Models.DTOs.Calificacion;
+using profefolio.Repository;
+
+namespace profefolio.Services;
+
+public class CalificacionService : ICalificacion
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public PlanillaDTO GetAll(int idMateriaLista)
+    {
+        throw new NotImplementedException();
+    }
+}
