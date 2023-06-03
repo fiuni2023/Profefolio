4aa671bbf71926bc3c09b0211ee697073f3fca69
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-03 18:27:07 -0400

    Agregado Referencia

--- profefolio/Models/DTOs/Calificacion/AlumnoWithPuntajesDTO.cs
+++ profefolio/Models/DTOs/Calificacion/AlumnoWithPuntajesDTO.cs
@@ -4,8 +4,9 @@ public class AlumnoWithPuntajesDTO
 {
     public double PorcentajeTotalLogrado { get; set; }
     public double PuntajeTotalLogrado { get; set; }
-    public int Alumno { get; set; }
+    public int AlumnoId { get; set; }
     public string? Nombre { get; set; }
     public string? Apellido { get; set; }
     public string? Doc { get; set; }
+    public List<PuntajeDTO>? Puntajes { get; set; }
 }
