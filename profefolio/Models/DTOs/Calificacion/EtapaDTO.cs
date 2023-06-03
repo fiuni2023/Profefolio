e68ec4157c12bb8f3f262a1a0c42de9b6c3acaa5
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-03 18:54:11 -0400

    Gestion de Etapas

--- profefolio/Models/DTOs/Calificacion/EtapaDTO.cs
+++ profefolio/Models/DTOs/Calificacion/EtapaDTO.cs
@@ -3,5 +3,5 @@ namespace profefolio.Models.DTOs.Calificacion;
 public class EtapaDTO
 {
     public string? Etapa { get; set; }
-    private List<AlumnoWithPuntajesDTO>? Alumnos { get; set; }
+    public List<AlumnoWithPuntajesDTO>? Alumnos { get; set; }
 }
--- profefolio/Services/CalificacionService.cs
+++ profefolio/Services/CalificacionService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using profefolio.Models;
 using profefolio.Models.DTOs.Calificacion;
 using profefolio.Repository;
@@ -33,6 +34,35 @@ public class CalificacionService : ICalificacion
 
     public PlanillaDTO GetAll(int idMateriaLista)
     {
+        var evaluacionesQuery = _db.Eventos
+                .Include(c => c.EvaluacionAlumnos)
+                .Include(c => c.MateriaList)
+                .Where(c => c.Deleted);
+
+        
+        var result = new PlanillaDTO
+        {
+            MateriaId = idMateriaLista,
+            Etapas = new List<EtapaDTO>()
+        };
+
+        foreach (var evaluacion in evaluacionesQuery)
+        {
+            var etapa = new EtapaDTO
+            {
+                Etapa = evaluacion.Etapa,
+                Alumnos = new List<AlumnoWithPuntajesDTO>()
+            };
+            
+            result.Etapas.Add(etapa);
+        }
+
         throw new NotImplementedException();
+    }
+
+    //TODO Validar Si algun alumno se integra luego de empezar las clases
+    private void ValidarPlanilla(int idMateriaLista)
+    {
+        
     }
 }
