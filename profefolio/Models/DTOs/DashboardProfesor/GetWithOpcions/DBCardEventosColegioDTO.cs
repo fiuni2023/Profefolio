971161620f6e609a871d140514695e7cbdf6a2ef
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2023-06-02 23:21:59 -0400

    se crea DBCardEventosColegioDTO

---
+++ profefolio/Models/DTOs/DashboardProfesor/GetWithOpcions/DBCardEventosColegioDTO.cs
@@ -0,0 +1,12 @@
+namespace profefolio.Models.DTOs.DashboardProfesor.GetWithOpcions;
+using Newtonsoft.Json;
+
+    public class DBCardEventosColegioDTO : DataDTO
+    {
+       
+        public string Tipo { get; set; } = "";
+        public DateTime Fecha { get; set; }
+        public string nombreMateria { get; set; } = "";
+        public string nombreClase { get; set; } = "";
+    
+    }
