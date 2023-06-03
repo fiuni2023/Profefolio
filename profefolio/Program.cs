ce783030689c15193e65aaf25cad2c5d6bdb344d
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-03 18:29:39 -0400

    Inyeccion de Dependencias

--- profefolio/Program.cs
+++ profefolio/Program.cs
@@ -101,6 +101,7 @@ builder.Services.AddScoped<IEvento, EventoService>();
 builder.Services.AddScoped<IAsistencia, AsistenciaService>();
 builder.Services.AddScoped<IAnotacion, AnotacionesService>();
 builder.Services.AddScoped<IDocumento, DocumentoService>();
+builder.Services.AddScoped<ICalificacion, CalificacionService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
