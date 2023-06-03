cc02cbdcf362a6c79ebc7495d3baa3e1d80c9c51
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-03 18:41:01 -0400

    Disposed Implementado

--- profefolio/Services/CalificacionService.cs
+++ profefolio/Services/CalificacionService.cs
@@ -7,15 +7,29 @@ namespace profefolio.Services;
 public class CalificacionService : ICalificacion
 {
     private ApplicationDbContext _db;
+    private bool _disposedValue; 
 
     public CalificacionService(ApplicationDbContext db)
     {
         _db = db;
+        _disposedValue = false;
     }
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposedValue) return;
+        if (disposing)
+        {
+            _db.Dispose();
+        }
+        _disposedValue = true;
+    }
+
     public void Dispose()
     {
-        throw new NotImplementedException();
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
+
 
     public PlanillaDTO GetAll(int idMateriaLista)
     {
