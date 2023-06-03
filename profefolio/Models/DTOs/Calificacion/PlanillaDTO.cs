04aabcccf51807652138febf75fe2b93648b0729
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-03 18:30:11 -0400

    Nullable true

--- profefolio/Models/DTOs/Calificacion/PlanillaDTO.cs
+++ profefolio/Models/DTOs/Calificacion/PlanillaDTO.cs
@@ -3,6 +3,6 @@ namespace profefolio.Models.DTOs.Calificacion;
 public class PlanillaDTO
 {
     public int MateriaId { get; set; }
-    public string Materia { get; set; }
-    public List<EtapaDTO> Etapas { get; set; }
+    public string? Materia { get; set; }
+    public List<EtapaDTO>? Etapas { get; set; }
 }
