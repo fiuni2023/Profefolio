225db7b4a88cd12fd98ff95612d8d1f19311b939
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-04 10:27:07 -0400

    Nueva verificacion

--- profefolio/Services/CalificacionService.cs
+++ profefolio/Services/CalificacionService.cs
@@ -36,10 +36,64 @@ public class CalificacionService : ICalificacion
 
     public PlanillaDTO GetAll(int idMateriaLista)
     {
-        var evaluacionesQuery = _db.ClasesAlumnosColegios
-            .Where(c => !c.Deleted);
+        var alumnosQuery = _db.ClasesAlumnosColegios
+            .Include(c => c.Evaluaciones)
+            .ThenInclude(x => x.Evaluacion)
+            .ThenInclude(x => x.MateriaList)
+            .Where(c => !c.Deleted)
+            .Where(c => c.Evaluaciones
+                .Any(y => y.Evaluacion.MateriaListaId == idMateriaLista));
+
+        var materia = _db.MateriaListas
+            .Include(m => m.Materia)
+            .Where(m => !m.Deleted && m.Id == idMateriaLista)
+            .Select(m => m.Materia.Nombre_Materia)
+            .First();
             
-        return null;
+        var planilla = new PlanillaDTO
+        {
+            MateriaId = idMateriaLista,
+            Materia = materia,
+            Etapas = new List<EtapaDTO>()
+        };
+
+        foreach (var alumno in alumnosQuery)
+        {
+            var evaluacionesAlumnos = alumno.Evaluaciones;
+
+            if (evaluacionesAlumnos.IsNullOrEmpty())
+            {
+                
+            }
+            foreach (var evaluacionAlumno in evaluacionesAlumnos)
+            {
+                
+            }
+        }
+
+
+        return planilla;
+    }
+
+    private void CargarEvaluaciones(ClasesAlumnosColegio cac)
+    {
+        var cantEvaluaciones = _db.ClasesAlumnosColegios
+            .Include(c => c.Evaluaciones)
+            .Where(c => !c.Deleted && cac.Id != c.Id)
+            .Where(c => c.Evaluaciones.Any())
+            .Select(c => c.Evaluaciones)
+            .First()
+            .Count;
+
+
+        for (var i = 0; i < cantEvaluaciones; i++)
+        {
+            cac.Evaluaciones.Add(new EvaluacionAlumno()
+            {
+                ClasesAlumnosColegioId = cac.Id,
+                
+            });
+        }
     }
 
    
