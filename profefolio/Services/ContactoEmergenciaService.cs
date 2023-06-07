c9b317dabd240f7b6f492015640c3a86a563a11f
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-06-07 06:18:08 -0400

    se creo el service del modelo de contactoemergencia

---
+++ profefolio/Services/ContactoEmergenciaService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using profefolio.Models.Entities;
+using profefolio.Repository;
+
+namespace profefolio.Services
+{
+    public class ContactoEmergenciaService : IContactoEmergencia
+    {
+        public Task<ContactoEmergencia> Add(ContactoEmergencia t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContactoEmergencia Edit(ContactoEmergencia t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContactoEmergencia> FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ContactoEmergencia> GetAll(int page, int cantPorPag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
