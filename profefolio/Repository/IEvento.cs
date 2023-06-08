b7763e9655f9b896f8863c0cdade474972162e37
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 23:36:30 -0400

    Interface

--- profefolio/Repository/IEvento.cs
+++ profefolio/Repository/IEvento.cs
@@ -4,11 +4,6 @@ namespace profefolio.Repository;
 
 public interface IEvento : IDisposable
 {
-    Task<Evaluacion> FindByEventoRepetido(String tipo, DateTime fecha, int clase, int materia, int colegio);
-    Task<List<Evaluacion>> GetAll(String prfId);
-    Task<Evaluacion> FindById2(int id);
     Task<Evaluacion> Add(Evaluacion e, string user);
-    Evaluacion Edit(Evaluacion e);
     Task Save();
-    int Count();
 }
