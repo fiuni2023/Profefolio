2998c14d159cbb1fb8400a62496445de03259fed
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-06-08 08:23:41 -0400

    se agrego el metodo post de creacion

--- profefolio/Controllers/AnotacionAlumnoController.cs
+++ profefolio/Controllers/AnotacionAlumnoController.cs
@@ -10,6 +10,7 @@ using profefolio.Repository;
 using profefolio.Models.DTOs.AnotacionAlumno;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using profefolio.Models.Entities;
 
 namespace profefolio.Controllers
 {
@@ -29,7 +30,7 @@ namespace profefolio.Controllers
 
         [HttpPost("/get")]
         [Authorize(Roles = "Profesor")]
-        public async Task<IActionResult> GetAll([FromBody] AnotacionAlumnoGetDTO dto)
+        public async Task<ActionResult<List<AnotacionAlumnoResultDTO>>> GetAll([FromBody] AnotacionAlumnoGetDTO dto)
         {
             try
             {
@@ -52,5 +53,33 @@ namespace profefolio.Controllers
             }
         }
 
+
+        [HttpPost]
+        [Authorize(Roles = "Profesor")]
+        public async Task<IActionResult> Post([FromBody]AnotacionAlumnoCreateDTO dto)
+        {
+            try{
+                // validar que el profesor sea profesor de la materia lista
+                // valdiar que el alumno sea de la clase de la materia
+                var userEmail = User.FindFirstValue(ClaimTypes.Name);
+
+                if(await _anotAlumnoService.ValidarDatos(dto.MateriaListaId, userEmail, dto.AlumnoId)){
+                    var model = _mapper.Map<AnotacionAlumno>(dto);
+                    model.Created = DateTime.Now;
+                    model.CreatedBy = userEmail;
+                    model.Deleted = false;
+
+                    await _anotAlumnoService.Add(model);
+                    await _anotAlumnoService.Save();
+                    return Ok();
+                }
+
+                return BadRequest("Datos incorrectos");
+
+            }catch(Exception e){
+                Console.WriteLine($"{e}");
+                return BadRequest("Error al tratar de guardar");
+            }
+        }
     }
 }
