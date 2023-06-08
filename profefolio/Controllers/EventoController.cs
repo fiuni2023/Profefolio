c562602dd56a849922af7413b8d4ce59b1371a4e
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 23:36:07 -0400

    Controller

--- profefolio/Controllers/EventoController.cs
+++ profefolio/Controllers/EventoController.cs
@@ -38,102 +38,7 @@ namespace profefolio.Controllers
             _colegioProfesorService = colegioProfesorService;
             _profesorService = profesorService;
         }
-
         
-        /// <summary>
-        /// Retorna el evento creado por el prf que hace la petición.
-        /// Solo un profesor puede realizar la peticion
-        /// https://localhost:7063/api/Evento/{id[int]}
-        /// </summary>
-        /// 
-        /// {
-        /// "tipo": "Prueba sumatoria",
-        /// "fecha": "2023-05-13T03:14:14.061",
-        /// "nombreMateria": "Filosofia",
-        /// "nombreClase": "primero",
-        /// "nombreColegio": "Monseñor",
-        /// "id": 1
-        /// }
-        /// 
-        /// <remarks>
-        /// </remarks>
-        [HttpGet("{id}")]
-        [Authorize(Roles = "Profesor")]
-        public async Task<ActionResult<EventoResultFullDTO>> GetEvento(int id)
-        { 
-            /*
-            try
-            {
-                var evento = await _eventoService.FindById2(id);
-                if (evento == null)
-                {
-                    return BadRequest("Evento inexistente");
-                }
-
-                var userEmail = User.FindFirstValue(ClaimTypes.Name);
-                var profId = await _profesorService.GetProfesorIdByEmail(userEmail);
-
-                if (profId != evento.ProfesorId)
-                {
-                    return BadRequest("No puede acceder al evento");
-                }
-
-                var response = _mapper.Map<EventoResultFullDTO>(evento);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                _log.Error("An error occurred in the GetEvento method", ex);
-                return BadRequest("Error inesperado durante la busqueda");
-            }
-            */
-            return BadRequest();
-        }
-
-        /// <summary>
-        /// Retorna todos los eventos creados por el prf que hace la petición.
-        /// 
-        /// https://localhost:7063/api/Evento
-        /// </summary>
-        /// [
-        /// {
-        /// "tipo": "Prueba sumatoria",
-        /// "fecha": "2023-05-13T03:14:14.061",
-        /// "nombreMateria": "Filosofia",
-        /// "nombreClase": "primero",
-        /// "nombreColegio": "Monseñor",
-        /// "id": 1
-        /// }
-        /// ]
-        /// <remarks>
-        /// </remarks>
-        [HttpGet]
-        [Authorize(Roles = "Profesor")]
-        public async Task<ActionResult<List<EventoResultFullDTO>>> GetAll()
-        {
-            try
-            {
-                var userEmail = User.FindFirstValue(ClaimTypes.Name);
-                var profId = await _profesorService.GetProfesorIdByEmail(userEmail);
-
-                var eventos = await _eventoService.GetAll(profId);
-                return Ok(_mapper.Map<List<EventoResultFullDTO>>(eventos));
-            }
-            catch (Exception e)
-            {
-                 _log.Error("An error occurred in the GetAll method", e);
-                return BadRequest("Error inesperado durante la busqueda");
-            }
-        }
-
-        /// <summary>
-        /// Guarda un Evento. 
-        /// Se recibe tipo evento, fecha, materia, clase, colegio
-        /// https://localhost:7063/api/Evento
-        /// </summary>
-        /// <response code="200">Retorna un status 200 vacio</response>
-        /// <remarks>
-        /// </remarks>
         [HttpPost]
         [Authorize(Roles = "Profesor")]
         public async Task<ActionResult<EventoResultDTO>> PostEvento([FromBody] EventoDTO evento)
@@ -143,72 +48,36 @@ namespace profefolio.Controllers
             {
                 return BadRequest("Objeto No valido");
             }
-            if (!(evento.Tipo.Equals("Examen") || evento.Tipo.Equals("Parcial")
-            || evento.Tipo.Equals("Prueba sumatoria") || evento.Tipo.Equals("Evento")))
+
+            var evalue = evento.Tipo != null && !(evento.Tipo.Equals("Examen")
+                                                  || evento.Tipo.Equals("Parcial")
+                                                  || evento.Tipo.Equals("Prueba sumatoria")
+                                                  || evento.Tipo.Equals("Evento"));
+            if (evalue)
             {
                 return BadRequest("Tipo de evento invalido");
             }
 
-            if (evento.MateriaId == null || evento.Fecha == null || evento.ClaseId == null
-            || evento.ColegioId == null || evento.Tipo == null)
-            {
-                return BadRequest("Datos no validos");
-            }
-
-            //VERIFICAR ID materia
-            var materia = await _materiaService.FindById(evento.MateriaId);
-            if (materia == null)
-            {
-                return BadRequest($"Datos invalidos.");
-            }
-            //VERIFICAR ID clase
-            var clase = await _claseService.FindById(evento.ClaseId);
-            if (clase == null)
-            {
-                return BadRequest($"Datos invalidos.");
-            }
-            //VERIFICAR ID colegio
-            var colegio = await _colegioService.FindById(evento.ColegioId);
-            if (colegio == null)
-            {
-                return BadRequest($"Datos invalidos.");
-            }
-            //verificar que no se agregue un evento a una clase que no sea del profesor logueado
-            var userEmail = User.FindFirstValue(ClaimTypes.Name);
-
-            var profId = await _profesorService.GetProfesorIdByEmail(userEmail);
-            evento.ProfesorId = profId;
-
-            if (clase.ColegioId != evento.ColegioId)
-            {
-                return BadRequest("No puede matipular datos ajenos.");
-            }
+            var user = User.FindFirstValue(ClaimTypes.Name);
+            
             //verificar que el prf pertenezca al colegio al cual quiere agregar un evento
-            var exist = _colegioProfesorService.Count(evento.ColegioId, userEmail);
+            var exist = _colegioProfesorService.Count(evento.ColegioId, user);
             var resultado = exist.Result;
             if (resultado == 0)
             {
                 return BadRequest("No puede agregar eventos en otros colegios.");
             }
 
-            //verificar que no haya el mismo tipo de evento la misma fecha en la misma materia de una clase
-            var verificarEvento = await _eventoService.FindByEventoRepetido(evento.Tipo, evento.Fecha,
-            evento.ClaseId, evento.MateriaId, evento.ColegioId);
-            if (verificarEvento != null)
-            {
-                return BadRequest($"Ya existe el evento.");
-            }
+           
             try
             {
                 var p = _mapper.Map<Evaluacion>(evento);
-                p.CreatedBy = userEmail;
+                p.CreatedBy = user;
                 p.Deleted = false;
-                
-                var user = User.FindFirstValue(ClaimTypes.Name);
                 var saved = await _eventoService.Add(p, user);
                 await _eventoService.Save();
 
-                return Ok(_mapper.Map<EventoResultDTO>(saved));
+                return Ok("Guardado");
             }
             catch (BadHttpRequestException e)
             {
