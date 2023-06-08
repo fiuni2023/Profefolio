1fb84f75b31f7c0cf283007ca0f79068c9d2a34e
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 22:00:42 -0400

    Agregado al Interface Put

--- profefolio/Repository/ICalificacion.cs
+++ profefolio/Repository/ICalificacion.cs
@@ -6,4 +6,5 @@ public interface ICalificacion : IDisposable
 { 
     Task<PlanillaDTO> GetAll(int idMateriaLista, string user);
     Task<bool> Verify(int idMateriaLista, string user);
+    Task<PlanillaDTO> Put(int idMateriaLista, CalificacionPutDto dto, string user);
 }
