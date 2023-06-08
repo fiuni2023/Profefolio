75a87c74f5244ce3ce3c0ab45e504ed4abf17c59
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 23:36:19 -0400

    Service

--- profefolio/Services/EventoService.cs
+++ profefolio/Services/EventoService.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using profefolio.Migrations;
 using profefolio.Models;
 using profefolio.Models.Entities;
 using profefolio.Repository;
@@ -15,58 +14,7 @@ public class EventoService : IEvento
     {
         _dbContext = dbContext;
     }
-    public async Task<Evaluacion> FindById(int id)
-    {
-        return await _dbContext.Eventos
-            .Where(p => !p.Deleted && p.Id == id)
-            .FirstOrDefaultAsync();
-    }
-    public async Task<Evaluacion> FindById2(int id)
-    {
-        
-        return await _dbContext.Eventos
-            .Where(p => !p.Deleted && p.Id == id)
-            .Include(e => e.MateriaList)
-            .FirstOrDefaultAsync();
-            
-        
-    }
-
-
-    public async Task<Evaluacion> FindByEventoRepetido(String t, DateTime f, int c, int m, int col)
-    {
-        
-        return await _dbContext.Eventos
-            .Include(e => e.MateriaList)
-            .ThenInclude(c => c.Clase)
-            .Where(p => p.MateriaList != null && !p.Deleted && p.Tipo == t && p.Fecha == f 
-                        && p.MateriaList.MateriaId == m && p.MateriaList.ClaseId == c && 
-                        p.MateriaList.Clase.ColegioId == col)
-            .FirstOrDefaultAsync();
-        
-        throw new NotImplementedException();
-    }
-
-    public async Task<List<Evaluacion>> GetAll(String prfId)
-    {
-        /*
-        return await _dbContext.Eventos
-        .Where(p => !p.Deleted)
-        .Where(p => p.ProfesorId == prfId)
-        .Include(e => e.Materias)
-        .Include(e => e.Clases)
-        .Include(e => e.Colegios)
-        .ToListAsync();
-        */
-        throw new NotImplementedException();
-    }
-
-    public Evaluacion Edit(Evaluacion t)
-    {
-        _dbContext.Entry(t).State = EntityState.Modified;
-        return t;
-    }
-
+    
     public async Task<Evaluacion> Add(Evaluacion t, string user)
     {
         var profesor =await _dbContext.Users
@@ -120,17 +68,7 @@ public class EventoService : IEvento
     {
         await _dbContext.SaveChangesAsync();
     }
-
-    public int Count()
-    {
-        return _dbContext.Eventos
-            .Count(p => !p.Deleted);
-    }
-
-    public bool Exist()
-    {
-        return Count() > 0;
-    }
+    
     protected virtual void Dispose(bool disposing)
     {
         if (!this._disposed)
@@ -148,13 +86,4 @@ public class EventoService : IEvento
         Dispose(true);
         GC.SuppressFinalize(this);
     }
-
-    public IEnumerable<Evaluacion> GetAll(int page, int cantPorPag)
-    {
-        return _dbContext.Eventos
-         .Where(p => !p.Deleted)
-         .Skip(page * cantPorPag)
-         .Take(cantPorPag);
-    }
-
 }
