53a3edd2d0996054b2dc56e7e3e05e3fbb4a304a
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 23:06:18 -0400

    Automatizado el Add

--- profefolio/Services/EventoService.cs
+++ profefolio/Services/EventoService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using profefolio.Migrations;
 using profefolio.Models;
 using profefolio.Models.Entities;
 using profefolio.Repository;
@@ -66,8 +67,51 @@ public class EventoService : IEvento
         return t;
     }
 
-    public async Task<Evaluacion> Add(Evaluacion t)
+    public async Task<Evaluacion> Add(Evaluacion t, string user)
     {
+        var profesor =await _dbContext.Users
+            .Where(p => !p.Deleted)
+            .FirstOrDefaultAsync(p => p.Email.Equals(user));
+
+        var verifyProfe = await _dbContext.MateriaListas
+            .AnyAsync(ml => !ml.Deleted && ml.Id == t.MateriaListaId);
+        
+        
+            
+        
+        
+        if (!verifyProfe || profesor == null)
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        var claseId = await _dbContext.MateriaListas
+            .Where(ml => !ml.Deleted)
+            .Where(ml => ml.ProfesorId.Equals(profesor.Id) && ml.Id == t.MateriaListaId)
+            .Select(c => c.ClaseId)
+            .FirstOrDefaultAsync();
+
+
+        var alumnosQuery = _dbContext.ClasesAlumnosColegios
+            .Where(cac => !cac.Deleted)
+            .Where(cac => cac.ClaseId == claseId);
+
+        t.EvaluacionAlumnos = new List<EvaluacionAlumno>();
+
+        foreach (var alumno in alumnosQuery)
+        {
+            var evaluacion = new EvaluacionAlumno
+            {
+                PuntajeLogrado = 0,
+                PorcentajeLogrado = 0,
+                ClasesAlumnosColegioId = alumno.Id,
+                Created = DateTime.Now,
+                CreatedBy = user
+            };
+            
+            t.EvaluacionAlumnos.Add(evaluacion);
+        }
+        
         var result = await _dbContext.Eventos.AddAsync(t);
         return result.Entity;
     }
