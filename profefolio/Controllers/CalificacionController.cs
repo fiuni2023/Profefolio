1db230ecc7b77f4936f5204131afa9599bbea879
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 21:57:24 -0400

    CalificacionController.cs

---
+++ profefolio/Controllers/CalificacionController.cs
@@ -0,0 +1,42 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using profefolio.Models.DTOs.Calificacion;
+using profefolio.Repository;
+
+namespace profefolio.Controllers;
+
+[Route("api/{controller}")]
+[Authorize(Roles = "Profesor")]
+public class CalificacionController : ControllerBase
+{
+    private ICalificacion _calificacionService;
+
+    public CalificacionController(ICalificacion calificacionService)
+    {
+        _calificacionService = calificacionService;
+    }
+
+    [Route("{idMateriaLista:int}")]
+    public async Task<ActionResult<EtapaDTO>> Get(int idMateriaLista)
+    {
+        try
+        {
+            var user = User.FindFirstValue(ClaimTypes.Name);
+            await _calificacionService.Verify(idMateriaLista, user);
+
+            var result = await _calificacionService.GetAll(idMateriaLista, user);
+            return Ok(result);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            Console.WriteLine(e);
+            return Unauthorized();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest("Error al realizar la peticion, revisa el Log");
+        }
+    }
+}
