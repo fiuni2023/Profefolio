9ef54e66e782fd3448909110b6dc2382e4f2742b
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-07 22:47:48 -0400

    Agregado y modificados las funciones en la interfaz

--- profefolio/Repository/IEvento.cs
+++ profefolio/Repository/IEvento.cs
@@ -2,9 +2,13 @@ using profefolio.Models.Entities;
 
 namespace profefolio.Repository;
 
-public interface IEvento : IRepository<Evaluacion>
+public interface IEvento : IDisposable
 {
     Task<Evaluacion> FindByEventoRepetido(String tipo, DateTime fecha, int clase, int materia, int colegio);
     Task<List<Evaluacion>> GetAll(String prfId);
     Task<Evaluacion> FindById2(int id);
+    Task<Evaluacion> Add(Evaluacion e, string user);
+    Evaluacion Edit(Evaluacion e);
+    Task Save();
+    int Count();
 }
