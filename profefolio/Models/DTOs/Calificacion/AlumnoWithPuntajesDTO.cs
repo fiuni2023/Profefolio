e287daeb03adc8080ebc20dc20db6b8bae77fa4a
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-10 14:10:52 -0400

    Modificado

--- profefolio/Models/DTOs/Calificacion/AlumnoWithPuntajesDTO.cs
+++ profefolio/Models/DTOs/Calificacion/AlumnoWithPuntajesDTO.cs
@@ -1,12 +1,11 @@
 namespace profefolio.Models.DTOs.Calificacion;
 
+// ReSharper disable once InconsistentNaming
 public class AlumnoWithPuntajesDTO
 {
-    public double PorcentajeTotalLogrado { get; set; }
-    public double PuntajeTotalLogrado { get; set; }
     public int AlumnoId { get; set; }
     public string? Nombre { get; set; }
     public string? Apellido { get; set; }
     public string? Doc { get; set; }
-    public List<PuntajeDTO>? Puntajes { get; set; }
+    public List<EtapaDTO>? Etapas { get; set; }
 }
