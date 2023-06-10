db2b4b06c330dedae5dfba7d0a14bff87e02602e
CarlosTorres1999 <carlos.torres@fiuni.edu.py>
2023-06-10 02:28:27 -0400

    Var Puntaje agregado

--- profefolio/Models/DTOs/Evento/EventoDTO.cs
+++ profefolio/Models/DTOs/Evento/EventoDTO.cs
@@ -1,14 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace profefolio.Models.DTOs.Evento;
 using Newtonsoft.Json;
 
+// ReSharper disable once InconsistentNaming
 public class EventoDTO
 {
+     [Required(ErrorMessage = "Requerido")]
      public string? Tipo { get; set; }
+     [Required(ErrorMessage = "Requerido")]
      public string? Etapa { get; set; }
+     [Required(ErrorMessage = "Requerido")]
      public DateTime Fecha { get; set; }
+     [Required(ErrorMessage = "Requerido")]
      public int MateriaId{ get; set; }
+     [Required(ErrorMessage = "Requerido")]
      public int ClaseId { get; set; }
+     [Required(ErrorMessage = "Requerido")]
      public int ColegioId { get; set; }
+     [Required(ErrorMessage = "Requerido")]
      public string? ProfesorId { get; set; }
-    
+     public double Puntaje { get; set; } = 0;
+
 }
