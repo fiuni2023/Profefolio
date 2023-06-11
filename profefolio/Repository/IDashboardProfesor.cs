5b234dc8a11f234d3ff5bb2255d6ddb199052b9a
lorenaosuch2021 <lorena.osuch2019@fiuni.edu.py>
2023-06-11 17:46:14 -0400

    se obtienen info para cards materia

--- profefolio/Repository/IDashboardProfesor.cs
+++ profefolio/Repository/IDashboardProfesor.cs
@@ -24,7 +24,7 @@ namespace profefolio.Repository
 
         // horarios de materias en cada clase
         Task<List<HorasCatedrasMaterias>> FindAllHorariosClasesByEmailProfesorAndIdColegio(int idColegio, string email, int anho); 
-        Task<MateriaLista> FindDataForCardOfInfoMateria(int idMateriaLista, string emailProfesor);
+        Task<DBCardsMateriaInfo> FindDataForCardOfInfoMateria(int idMateriaLista, string emailProfesor);
         Task<MateriaLista> GetPromediosPuntajesByIdMateriaLista(int idMateriaLista, string emailProfesor);
         Task<(double, double, double)> GetPromedioAsistenciasByMonth(int year, int month, int idMateriaLista, string profesorId);
         Task<List<DBCardEventosColegioDTO>> FindEventosOfClase(String idprofesor, int idColegio);
