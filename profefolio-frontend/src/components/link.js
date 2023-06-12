aac50c6c66d3d0f8c4573601d66ee7b50c24ecb1
dahybarboza <126834904+dahybarboza@users.noreply.github.com>
2023-06-12 00:40:34 -0400

    Cambios

--- profefolio-frontend/src/components/link.js
+++ profefolio-frontend/src/components/link.js
@@ -1,2 +1,4 @@
-const APILINK = 'https://profefolio-develop.azurewebsites.net'
+//const APILINK = 'https://profefolio-develop.azurewebsites.net'
+const APILINK = 'https://localhost:7063'
+
 export default APILINK
